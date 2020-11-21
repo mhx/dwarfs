49a96f9b6ca7145b720b6b38b1dd0f80d73a4cf3
Marcus Holland-Moritz <github@mhxnet.de>
2020-11-21 19:10:51 +0100

    Clean up Lua stuff

--- dwarfs.lua
+++ dwarfs.lua
@@ -1,3 +1,21 @@
+--
+-- Copyright (c) Marcus Holland-Moritz
+--
+-- This file is part of dwarfs.
+--
+-- dwarfs is free software: you can redistribute it and/or modify it under the
+-- terms of the GNU General Public License as published by the Free Software
+-- Foundation, either version 3 of the License, or (at your option) any later
+-- version.
+--
+-- dwarfs is distributed in the hope that it will be useful, but WITHOUT ANY
+-- WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+-- A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License along with
+-- dwarfs.  If not, see <https://www.gnu.org/licenses/>.
+--
+
 function filter(f)
   -- if f.name == 'Jamroot' or (f.name == 'test' and f.type == 'dir') then
   --   return false
