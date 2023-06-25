8a560d0bda8d7a1834d7ff1bbc7aed9bb00539ec
Marcus Holland-Moritz <github@mhxnet.de>
2023-06-25 20:17:15 +0200

    Add batch script for Windows build

---
+++ cmake/win.bat
@@ -0,0 +1,1 @@
+cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release -DWITH_TESTS=ON -DCMAKE_TOOLCHAIN_FILE=C:\Users\mhx\git\vcpkg\scripts\buildsystems\vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows-static -DVCPKG_INSTALLED_DIR=C:\Users\mhx\git\@vcpkg-install-dwarfs
