367c80559a66a3523294efe707236d4948ef9c95
Marcus Holland-Moritz <github@mhxnet.de>
2023-06-26 10:59:07 +0200

    More flexible batch file for Windows builds

--- cmake/win.bat
+++ cmake/win.bat
@@ -1,1 +1,10 @@
-cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release -DWITH_TESTS=ON -DCMAKE_TOOLCHAIN_FILE=C:\Users\mhx\git\vcpkg\scripts\buildsystems\vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows-static -DVCPKG_INSTALLED_DIR=C:\Users\mhx\git\@vcpkg-install-dwarfs
+@ECHO OFF
+
+SET build_mode=%1
+SHIFT
+
+IF "%build_mode%"=="" (
+  SET build_mode=Release
+)
+
+cmake .. -GNinja -DCMAKE_BUILD_TYPE=%build_mode% -DWITH_TESTS=ON -DCMAKE_TOOLCHAIN_FILE=C:\Users\mhx\git\vcpkg\scripts\buildsystems\vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows-static -DVCPKG_INSTALLED_DIR=C:\Users\mhx\git\@vcpkg-install-dwarfs
