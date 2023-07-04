b25d52a40e398078176173effb5172596433216d
Marcus Holland-Moritz <github@mhxnet.de>
2023-07-04 08:27:54 +0200

    Make dwarfs-universal usable without symlinks

--- src/universal.cpp
+++ src/universal.cpp
@@ -20,33 +20,68 @@
  */
 
 #include <filesystem>
+#include <iostream>
+#include <map>
 #include <string_view>
-#include <unordered_map>
+#include <vector>
+
+#include <folly/String.h>
+#include <folly/gen/String.h>
 
 #include "dwarfs/error.h"
+#include "dwarfs/tool.h"
 #include "dwarfs_tool_main.h"
 
 namespace {
 
 using namespace dwarfs;
 
-std::unordered_map<std::string_view, int (*)(int, char**)> const functions{
+std::map<std::string_view, int (*)(int, char**)> const functions{
+    {"dwarfs", &dwarfs_main},
     {"mkdwarfs", &mkdwarfs_main},
     {"dwarfsck", &dwarfsck_main},
     {"dwarfsextract", &dwarfsextract_main},
-    {"dwarfsbench", &dwarfsbench_main},
+    // {"dwarfsbench", &dwarfsbench_main},
 };
 
 } // namespace
 
 int main(int argc, char** argv) {
-  auto fun = &dwarfs::dwarfs_main;
+  if (argc > 1) {
+    std::string_view tool_arg(argv[1]);
+    if (tool_arg.starts_with("--tool=")) {
+      if (auto it = functions.find(tool_arg.substr(7)); it != functions.end()) {
+        std::vector<char*> argv_copy;
+        argv_copy.reserve(argc - 1);
+        argv_copy.emplace_back(argv[0]);
+        std::copy(argv + 2, argv + argc, std::back_inserter(argv_copy));
+        return dwarfs::safe_main(
+            [&] { return it->second(argc - 1, argv_copy.data()); });
+      }
+    }
+  }
+
+#ifndef _WIN32
   auto path = std::filesystem::path(argv[0]);
 
   if (auto it = functions.find(path.filename().string());
       it != functions.end()) {
-    fun = it->second;
+    return dwarfs::safe_main([&] { return it->second(argc, argv); });
   }
+#endif
 
-  return dwarfs::safe_main([&] { return fun(argc, argv); });
+  using namespace folly::gen;
+
+  auto tools = from(functions) | get<0>() | unsplit(", ");
+
+  // clang-format off
+  std::cout << tool_header("dwarfs-universal")
+            << "Command line options:\n"
+            << "  --tool=<name>                     "
+                 "which tool to run; available tools are:\n"
+            << "                                    "
+            << tools << "\n";
+  // clang-format on
+
+  return 0;
 }
