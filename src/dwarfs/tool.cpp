9eeb944e8c0c25e81b2bfc84cd4bea4c5fec8f90
Marcus Holland-Moritz <github@mhxnet.de>
2023-07-12 14:49:23 +0200

    Add shiny new tool header

--- src/dwarfs/tool.cpp
+++ src/dwarfs/tool.cpp
@@ -28,8 +28,15 @@ namespace dwarfs {
 
 std::string
 tool_header(std::string_view tool_name, std::string_view extra_info) {
-  return fmt::format("{} ({}{})\nbuilt on {}\n\n", tool_name, PRJ_GIT_ID,
-                     extra_info, PRJ_BUILD_ID);
+  return fmt::format(
+      // clang-format off
+    R"(     ___                  ___ ___)""\n"
+    R"(    |   \__ __ ____ _ _ _| __/ __|         Deduplicating Warp-speed)""\n"
+    R"(    | |) \ V  V / _` | '_| _|\__ \      Advanced Read-only File System)""\n"
+    R"(    |___/ \_/\_/\__,_|_| |_| |___/         by Marcus Holland-Moritz)""\n\n"
+      // clang-format on
+      "{} ({}{})\nbuilt on {}\n\n",
+      tool_name, PRJ_GIT_ID, extra_info, PRJ_BUILD_ID);
 }
 
 } // namespace dwarfs
