87be9b66c6cd1a61f0a6acac2ebbd991d6c94afa
Marcus Holland-Moritz <github@mhxnet.de>
2023-12-21 06:51:00 +0100

    chore: add shell script for audio file creation

---
+++ .maintainer-scripts/audio.sh
@@ -0,0 +1,14 @@
+for bits in 8 16 20 24 32; do
+  sox -D -r10000 -b$bits -n audio/test$bits.wav synth 5 sine ${bits}00
+  for ch in 1 2 3 4 5 6; do
+    sox -D -r10000 -b$bits -n audio/test$bits.$ch.wav synth 5 sine ${bits}${ch}0
+    sox -D -M audio/test$bits.?.wav audio/test$bits-$ch.wav
+  done
+  rm audio/test$bits.?.wav
+  for fmt in aiff caf w64; do
+    sox -D audio/test$bits.wav audio/test$bits.$fmt
+    for ch in 1 2 3 4 5 6; do
+      sox -D audio/test$bits-$ch.wav audio/test$bits-$ch.$fmt
+    done
+  done
+done
