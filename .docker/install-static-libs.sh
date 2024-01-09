bf1a59c4c811b8fb3950e7d5f544be1fd114cdab
Marcus Holland-Moritz <github@mhxnet.de>
2024-01-09 15:59:17 +0100

    chore: retry "file" ftp download as it's extremely unreliable

--- .docker/install-static-libs.sh
+++ .docker/install-static-libs.sh
@@ -6,16 +6,30 @@ cd "$HOME"
 mkdir pkgs
 cd pkgs
 
-LIBARCHIVE_VERSION=3.7.2
 FILE_VERSION=5.45
+FILE_SHA512=12611a59ff766c22a55db4b4a9f80f95a0a2e916a1d8593612c6ead32c247102a8fdc23693c6bf81bda9b604d951a62c0051e91580b1b79e190a3504c0efc20a
+LIBARCHIVE_VERSION=3.7.2
 FLAC_VERSION=1.4.3
 # BENCHMARK_VERSION=1.8.2
 
-export CC=clang-16
-export CXX=clang++-16
+export CC=clang-17
+export CXX=clang++-17
+
+RETRY=0
+while true; do
+    rm -f file-${FILE_VERSION}.tar.gz
+    wget ftp://ftp.astron.com/pub/file/file-${FILE_VERSION}.tar.gz
+    if echo "${FILE_SHA512}  file-${FILE_VERSION}.tar.gz" | sha512sum -c; then
+        break
+    fi
+    RETRY=$((RETRY+1))
+    if [ $RETRY -gt 10 ]; then
+        echo "Failed to download file-${FILE_VERSION}.tar.gz"
+        exit 1
+    fi
+done
 
 wget https://github.com/libarchive/libarchive/releases/download/v${LIBARCHIVE_VERSION}/libarchive-${LIBARCHIVE_VERSION}.tar.xz
-wget ftp://ftp.astron.com/pub/file/file-${FILE_VERSION}.tar.gz
 wget https://github.com/xiph/flac/releases/download/${FLAC_VERSION}/flac-${FLAC_VERSION}.tar.xz
 # wget https://github.com/google/benchmark/archive/refs/tags/v${BENCHMARK_VERSION}.tar.gz
 
