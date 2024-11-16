72180067d1d80bc3343d8ea0ac7444606cc2c1c0
Marcus Holland-Moritz <github@mhxnet.de>
2024-11-16 17:37:08 +0100

    feat: use glob-to-regex to simplify and enhance rule_based_entry_filter

--- src/writer/rule_based_entry_filter.cpp
+++ src/writer/rule_based_entry_filter.cpp
@@ -31,6 +31,8 @@
 #include <dwarfs/writer/entry_interface.h>
 #include <dwarfs/writer/rule_based_entry_filter.h>
 
+#include <dwarfs/internal/glob_to_regex.h>
+
 namespace dwarfs::writer {
 
 namespace internal {
@@ -51,11 +53,21 @@ struct filter_rule {
   };
 
   filter_rule(rule_type type, bool floating, std::string const& re,
-              std::string const& rule)
+              std::string_view rule, bool ignore_case = false)
       : type{type}
       , floating{floating}
-      , re{re}
+      , re{re, regex_flags(ignore_case)}
       , rule{rule} {}
+
+  static constexpr std::regex_constants::syntax_option_type
+  regex_flags(bool ignore_case) {
+    auto flags =
+        std::regex_constants::ECMAScript | std::regex_constants::optimize;
+    if (ignore_case) {
+      flags |= std::regex_constants::icase;
+    }
+    return flags;
+  }
 
   rule_type type;
   bool floating;
@@ -89,14 +101,34 @@ class rule_based_entry_filter_ : public rule_based_entry_filter::impl {
 
 template <typename LoggerPolicy>
 auto rule_based_entry_filter_<LoggerPolicy>::compile_filter_rule(
-    std::string_view rule_sv) -> filter_rule {
-  std::string rule{rule_sv};
+    std::string_view rule) -> filter_rule {
   std::string re;
   filter_rule::rule_type type;
 
-  auto* p = rule.c_str();
+  if (rule.empty()) {
+    throw std::runtime_error("empty filter rule");
+  }
 
-  switch (*p) {
+  auto splitpos = rule.find_first_of(' ');
+
+  if (splitpos == std::string::npos) {
+    throw std::runtime_error("invalid filter rule: " + std::string(rule));
+  }
+
+  auto pattern_start = rule.find_first_not_of(' ', splitpos);
+
+  if (pattern_start == std::string::npos) {
+    throw std::runtime_error("no pattern in filter rule: " + std::string(rule));
+  }
+
+  auto prefix = rule.substr(0, splitpos);
+  auto pattern = rule.substr(pattern_start);
+
+  if (prefix.empty()) {
+    throw std::runtime_error("no prefix in filter rule: " + std::string(rule));
+  }
+
+  switch (prefix[0]) {
   case '+':
     type = filter_rule::rule_type::include;
     break;
@@ -107,76 +139,34 @@ auto rule_based_entry_filter_<LoggerPolicy>::compile_filter_rule(
     throw std::runtime_error("rules must start with + or -");
   }
 
-  while (*++p == ' ')
-    ;
+  bool ignore_case{false};
+
+  prefix.remove_prefix(1);
+  for (auto opt : prefix) {
+    switch (opt) {
+    case 'i':
+      ignore_case = true;
+      break;
+
+    default:
+      throw std::runtime_error(
+          fmt::format("unknown option '{}' in filter rule: {}", opt, rule));
+    }
+  }
 
   // If the start of the pattern is not explicitly anchored, make it floating.
-  bool floating = *p && *p != '/';
+  bool floating = pattern[0] != '/';
 
   if (floating) {
     re += ".*/";
   }
 
-  while (*p) {
-    switch (*p) {
-    case '\\':
-      re += *p++;
-      if (p) {
-        re += *p++;
-      }
-      continue;
-
-    case '*': {
-      int nstar = 1;
-      while (*++p == '*') {
-        ++nstar;
-      }
-      switch (nstar) {
-      case 1:
-        if (re.ends_with('/') and (*p == '/' or *p == '\0')) {
-          re += "[^/]+";
-        } else {
-          re += "[^/]*";
-        }
-        break;
-      case 2:
-        re += ".*";
-        break;
-      default:
-        throw std::runtime_error("too many *s");
-      }
-    }
-      continue;
-
-    case '?':
-      re += "[^/]";
-      break;
-
-    case '.':
-    case '+':
-    case '^':
-    case '$':
-    case '(':
-    case ')':
-    case '{':
-    case '}':
-    case '|':
-      re += '\\';
-      re += *p;
-      break;
-
-    default:
-      re += *p;
-      break;
-    }
-
-    ++p;
-  }
+  re += dwarfs::internal::glob_to_regex_string(pattern) + "$";
 
   LOG_DEBUG << "'" << rule << "' -> '" << re << "' [floating=" << floating
-            << "]";
+            << ", ignore_case=" << ignore_case << "]";
 
-  return filter_rule(type, floating, re, rule);
+  return filter_rule(type, floating, re, rule, ignore_case);
 }
 
 template <typename LoggerPolicy>
