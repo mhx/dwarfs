a19145388ca9804bf7d0d21299dc1887c7d994e4
Marcus Holland-Moritz <github@mhxnet.de>
2025-03-29 15:04:07 +0100

    refactor: drop rvalue-ref `compress` overload

--- src/compression/brotli.cpp
+++ src/compression/brotli.cpp
@@ -69,12 +69,6 @@ class brotli_block_compressor final : public block_compressor::impl {
     return compressed;
   }
 
-  // NOLINTNEXTLINE(cppcoreguidelines-rvalue-reference-param-not-moved)
-  std::vector<uint8_t> compress(std::vector<uint8_t>&& data,
-                                std::string const* metadata) const override {
-    return compress(data, metadata);
-  }
-
   compression_type type() const override { return compression_type::BROTLI; }
 
   std::string describe() const override {
--- src/compression/ricepp.cpp
+++ src/compression/ricepp.cpp
@@ -130,12 +130,6 @@ class ricepp_block_compressor final : public block_compressor::impl {
     return compressed;
   }
 
-  // NOLINTNEXTLINE(cppcoreguidelines-rvalue-reference-param-not-moved)
-  std::vector<uint8_t> compress(std::vector<uint8_t>&& data,
-                                std::string const* metadata) const override {
-    return compress(data, metadata);
-  }
-
   compression_type type() const override { return compression_type::RICEPP; }
 
   std::string describe() const override {
