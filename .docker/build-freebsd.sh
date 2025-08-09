25a1d34a4cf75122447f45ed04bb13fd02256a84
Marcus Holland-Moritz <github@mhxnet.de>
2025-08-09 08:16:17 +0200

    chore: add CI for FreeBSD

---
+++ .docker/build-freebsd.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+set -eux
+
+export HOME=/root
+export PATH=/usr/local/bin:/usr/local/sbin:/bin:/usr/bin:/sbin:/usr/sbin
+export CCACHE_DIR=/ccache
+export RUNNER_TEMP=/runner_temp
+export RUNNER_WORKSPACE=/work
+export GITHUB_RUN_NUMBER="${GITHUB_RUN_NUMBER:-0}"
+export BUILD_MODE="${BUILD_MODE:-Release}"
+export CMAKE_ARGS="${CMAKE_ARGS:-}"
+
+# Source tarball path (NFS is nullfs-mounted from host)
+SRC_TARBALL="/mnt/opensource/artifacts/dwarfs/cache/dwarfs-source-${GITHUB_RUN_NUMBER}.tar.zst"
+test -r "${SRC_TARBALL}"
+
+cd "$HOME"
+rm -rf dwarfs dwarfs-*
+tar xf "${SRC_TARBALL}"
+ln -sfn dwarfs-* dwarfs
+
+rm -rf "${RUNNER_TEMP}/build"
+cmake --fresh \
+  -B"${RUNNER_TEMP}/build" \
+  -S"${HOME}/dwarfs" \
+  -DCMAKE_BUILD_TYPE="${BUILD_MODE}" \
+  -DWITH_TESTS=ON \
+  -DWITH_PXATTR=ON \
+  ${CMAKE_ARGS}
+
+cmake --build "${RUNNER_TEMP}/build" -j"$(sysctl -n hw.ncpu)"
+ctest --test-dir "${RUNNER_TEMP}/build" --output-on-failure -j"$(sysctl -n hw.ncpu)"
+cmake --build "${RUNNER_TEMP}/build" --target realclean
