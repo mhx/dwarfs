a137724ec4f0c7e02f5fe82a464468678d6f14c3
Marcus Holland-Moritz <github@mhxnet.de>
2025-08-12 09:05:55 +0200

    chore: bump binutils / gcc

--- .docker/install-toolchain.sh
+++ .docker/install-toolchain.sh
@@ -6,8 +6,8 @@ ARCH="$(uname -m)"
 OPTIMIZE_STR="${1:-2}"
 TARGET_ARCH_STR="${2:-$ARCH}"
 
-BINUTILS_VERSION=2.44
-GCC_VERSION=14.2.0
+BINUTILS_VERSION=2.45
+GCC_VERSION=15.2.0
 MUSL_VERSION=1.2.5
 LINUX_VERSION=6.15.7
 
@@ -34,45 +34,45 @@ cd gcc-${GCC_VERSION}
 for p in 0001-posix_memalign.patch \
          0002-gcc-poison-system-directories.patch \
          0003-specs-turn-on-Wl-z-now-by-default.patch \
-         0004-Turn-on-D_FORTIFY_SOURCE-2-by-default-for-C-C-ObjC-O.patch \
          0005-On-linux-targets-pass-as-needed-by-default-to-the-li.patch \
          0006-Enable-Wformat-and-Wformat-security-by-default.patch \
          0007-Enable-Wtrampolines-by-default.patch \
-         0008-Disable-ssp-on-nostdlib-nodefaultlibs-and-ffreestand.patch \
-         0009-Ensure-that-msgfmt-doesn-t-encounter-problems-during.patch \
-         0010-Don-t-declare-asprintf-if-defined-as-a-macro.patch \
-         0011-libiberty-copy-PIC-objects-during-build-process.patch \
-         0012-libgcc_s.patch \
-         0013-nopie.patch \
-         0014-ada-fix-shared-linking.patch \
-         0015-build-fix-CXXFLAGS_FOR_BUILD-passing.patch \
-         0016-add-fortify-headers-paths.patch \
-         0017-Alpine-musl-package-provides-libssp_nonshared.a.-We-.patch \
-         0018-DP-Use-push-state-pop-state-for-gold-as-well-when-li.patch \
-         0019-aarch64-disable-multilib-support.patch \
-         0020-s390x-disable-multilib-support.patch \
-         0021-ppc64-le-disable-multilib-support.patch \
-         0022-x86_64-disable-multilib-support.patch \
-         0023-riscv-disable-multilib-support.patch \
-         0024-always-build-libgcc_eh.a.patch \
-         0025-ada-libgnarl-compatibility-for-musl.patch \
-         0026-ada-musl-support-fixes.patch \
-         0027-configure-Add-enable-autolink-libatomic-use-in-LINK_.patch \
-         0028-configure-fix-detection-of-atomic-builtins-in-libato.patch \
-         0029-libstdc-do-not-throw-exceptions-for-non-C-locales-on.patch \
-         0030-gdc-unconditionally-link-libgphobos-against-libucont.patch \
-         0031-druntime-link-against-libucontext-on-all-platforms.patch \
-         0032-libgnat-time_t-is-always-64-bit-on-musl-libc.patch \
-         0033-libphobos-do-not-use-LFS64-symbols.patch \
-         0034-libgo-fix-lfs64-use.patch \
-         0035-loongarch-disable-multilib-support.patch \
-         0036-libphobos-add-riscv64-and-loongarch64-support.patch \
-         fix-arm64.patch \
-         ppc64le-quadmath.patch \
-         riscv64-improve-build-time.patch; do
-    fetch.sh https://gitlab.alpinelinux.org/alpine/aports/-/raw/3.21-stable/main/gcc/$p - | patch -p1
+         0008-gcc-disable-SSP-on-ffreestanding-nostdlib-and-nodefa.patch \
+         0009-gcc-params-set-default-ssp-buffer-size-to-4.patch \
+         0010-Ensure-that-msgfmt-doesn-t-encounter-problems-during.patch \
+         0011-Don-t-declare-asprintf-if-defined-as-a-macro.patch \
+         0012-libiberty-copy-PIC-objects-during-build-process.patch \
+         0013-libgcc_s.patch \
+         0014-nopie.patch \
+         0015-ada-fix-shared-linking.patch \
+         0016-build-fix-CXXFLAGS_FOR_BUILD-passing.patch \
+         0017-add-fortify-headers-paths.patch \
+         0018-Alpine-musl-package-provides-libssp_nonshared.a.-We-.patch \
+         0019-DP-Use-push-state-pop-state-for-gold-as-well-when-li.patch \
+         0020-aarch64-disable-multilib-support.patch \
+         0021-s390x-disable-multilib-support.patch \
+         0022-ppc64-le-disable-multilib-support.patch \
+         0023-x86_64-disable-multilib-support.patch \
+         0024-riscv-disable-multilib-support.patch \
+         0025-always-build-libgcc_eh.a.patch \
+         0026-ada-libgnarl-remove-use-of-glibc-specific-pthread_rw.patch \
+         0027-ada-libgnarl-use-posix_openpt-instead-of-glibc-speci.patch \
+         0028-ada-libgnarl-adaint-fix-sched.h-inclusion-for-musl.patch \
+         0029-configure-Add-enable-autolink-libatomic-use-in-LINK_.patch \
+         0030-configure-fix-detection-of-atomic-builtins-in-libato.patch \
+         0031-libstdc-do-not-throw-exceptions-for-non-C-locales-on.patch \
+         0032-gdc-unconditionally-link-libgphobos-against-libucont.patch \
+         0033-druntime-link-against-libucontext-on-all-platforms.patch \
+         0034-libgnat-time_t-is-always-64-bit-on-musl-libc.patch \
+         0035-libphobos-do-not-use-LFS64-symbols.patch \
+         0036-libgo-fix-lfs64-use.patch \
+         0037-loongarch-disable-multilib-support.patch \
+         0038-static-PIE-ensure-static-reaches-the-linker.patch \
+         0039-except-Don-t-use-the-cached-value-of-the-gcc_except_.patch \
+         0040-ada-libgnat-use-stub-symbolic-module-name-functions.patch \
+         0041-ada-libgnat-recognize-linux-musleabi-and-linux-muslg.patch; do
+    fetch.sh https://gitlab.alpinelinux.org/alpine/aports/-/raw/master/main/gcc/$p - | patch -p1
 done
-fetch.sh https://gcc.gnu.org/pipermail/gcc-patches/attachments/20250220/c6211b02/attachment.bin - | patch -p1
 
 ORIGPATH="$PATH"
 
