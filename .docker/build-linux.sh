818052344265e2c4e70f1b0e746bb855bb234c07
Marcus Holland-Moritz <github@mhxnet.de>
2025-08-17 12:34:05 +0200

    chore: configure coveralls as code coverage backup

--- .docker/build-linux.sh
+++ .docker/build-linux.sh
@@ -422,10 +422,11 @@ esac
 
 if [[ "-$BUILD_TYPE-" == *-coverage-* ]]; then
   rm -f /tmp-runner/dwarfs-coverage.txt
+  rm -f /tmp-runner/dwarfs-coverage.info
+  _objects="$(for i in mkdwarfs dwarfs dwarfsck dwarfsextract *_test *_tests ricepp/ricepp_test; do echo $i; done | sed -e's/^/-object=/')"
   llvm-profdata$CLANG_VERSION merge -sparse profile/* -o dwarfs.profdata
-  llvm-cov$CLANG_VERSION show -instr-profile=dwarfs.profdata \
-    $(for i in mkdwarfs dwarfs dwarfsck dwarfsextract *_test *_tests ricepp/ricepp_test; do echo $i; done | sed -e's/^/-object=/') \
-    >/tmp-runner/dwarfs-coverage.txt
+  llvm-cov$CLANG_VERSION export -format=lcov -compilation-dir="${GITHUB_WORKSPACE}" -instr-profile=dwarfs.profdata \
+      -ignore-filename-regex='(^|/)(fsst|folly|fbthrift)/' $_objects >/tmp-runner/dwarfs-coverage.lcov
 fi
 
 if [[ "-$BUILD_TYPE-" == *-static-* ]]; then
