1e48783d6d8e957ecbc0160d138ed9a26bd9205d
Marcus Holland-Moritz <github@mhxnet.de>
2025-08-19 12:12:00 +0200

    chore: speed up block merger tests when cross-compiling

--- test/block_merger_test.cpp
+++ test/block_merger_test.cpp
@@ -49,11 +49,15 @@ namespace {
 
 constexpr int const debuglevel{0};
 
+#ifdef DWARFS_TEST_CROSS_COMPILE
+constexpr size_t const max_runs_regular{25};
+constexpr size_t const max_runs_partial{5};
+constexpr size_t const num_runner_threads{4};
+#else
 constexpr size_t const max_runs_regular{250};
 constexpr size_t const max_runs_partial{50};
-constexpr size_t const max_runs_regular_quick{25};
-constexpr size_t const max_runs_partial_quick{5};
 constexpr size_t const num_runner_threads{16};
+#endif
 constexpr size_t const num_repetitions{4};
 
 struct block {
@@ -480,7 +484,7 @@ TEST(block_merger, random) {
 
   using merger_type = internal::multi_queue_block_merger<size_t, block>;
 
-  auto max_runs = skip_slow_tests() ? max_runs_regular_quick : max_runs_regular;
+  auto const max_runs = max_runs_regular;
 
   auto [passes, fails] = block_merger_test<merger_type>(max_runs);
 
@@ -495,7 +499,7 @@ TEST(block_merger, random_sized) {
       internal::multi_queue_block_merger<size_t, sized_block,
                                          sized_block_merger_policy>;
 
-  auto max_runs = skip_slow_tests() ? max_runs_regular_quick : max_runs_regular;
+  auto const max_runs = max_runs_regular;
 
   auto [passes, fails] = block_merger_test<merger_type>(max_runs);
 
@@ -510,7 +514,7 @@ TEST(block_merger, random_sized_partial) {
       internal::multi_queue_block_merger<size_t, sized_block,
                                          sized_block_merger_policy>;
 
-  auto max_runs = skip_slow_tests() ? max_runs_partial_quick : max_runs_partial;
+  auto const max_runs = max_runs_partial;
 
   auto [passes, fails] = block_merger_test<merger_type, true>(max_runs);
 
