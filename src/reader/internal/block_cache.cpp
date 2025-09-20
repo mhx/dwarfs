68c289d01137b648825b24ac66ae2e246f48d688
Marcus Holland-Moritz <github@mhxnet.de>
2025-09-20 17:42:07 +0200

    refactor: drop (no_)cache_image option
    This was best-effort anyway, since not issuing dontneed doesn't mean the
    mapping will actually be kept in memory.

--- src/reader/internal/block_cache.cpp
+++ src/reader/internal/block_cache.cpp
@@ -577,7 +577,7 @@ class block_cache_ final : public block_cache::impl {
 
       std::shared_ptr<cached_block> block = cached_block::create(
           LOG_GET_LOGGER, section, section.segment(mm_), buffer_factory_,
-          options_.mm_release, options_.disable_block_integrity_check);
+          options_.disable_block_integrity_check);
       blocks_created_.fetch_add(1, std::memory_order_relaxed);
 
       // Make a new set for the block
