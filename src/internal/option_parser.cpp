2d5b37065dea5234bb7c289b1f843be34141ac56
Marcus Holland-Moritz <github@mhxnet.de>
2025-10-01 12:45:06 +0200

    refactor: factor out and test `DWARFS_IOLAYER_OPTS` parsing

---
+++ src/internal/option_parser.cpp
@@ -0,0 +1,56 @@
+/* vim:set ts=2 sw=2 sts=2 et: */
+/**
+ * \author     Marcus Holland-Moritz (github@mhxnet.de)
+ * \copyright  Copyright (c) Marcus Holland-Moritz
+ *
+ * This file is part of dwarfs.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the “Software”), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <dwarfs/util.h>
+
+#include <dwarfs/internal/option_parser.h>
+
+namespace dwarfs::internal {
+
+option_parser::option_parser(std::string_view str)
+    : options_{parse_option_string(str)} {}
+
+std::optional<std::string_view> option_parser::get(std::string_view key) {
+  std::optional<std::string_view> result;
+  auto it = options_.find(key);
+  if (it != options_.end()) {
+    result.emplace(it->second);
+    options_.erase(it);
+  }
+  return result;
+}
+
+void option_parser::report_unused(
+    std::function<void(std::string_view, std::string_view)> const& report)
+    const {
+  for (auto const& [key, value] : options_) {
+    report(key, value);
+  }
+}
+
+} // namespace dwarfs::internal
