df8533ef550b67810170f2fd2f0473a7c6789656
Marcus Holland-Moritz <github@mhxnet.de>
2025-10-04 18:39:54 +0200

    feat(mmap_file_view): special handling when iterating over holes
    Instead of requesting "actual" hole data from the OS, we create a small
    read-only, zeroes mapping that will be re-used as long as it's large
    enough (and resized on demand). This is *much* faster than reading from
    the OS and will prevent pollution of the kernel file system cache.

--- src/internal/mmap_file_view.cpp
+++ src/internal/mmap_file_view.cpp
@@ -26,6 +26,11 @@
  * SPDX-License-Identifier: MIT
  */
 
+#include <algorithm>
+#include <bit>
+#include <cassert>
+#include <mutex>
+
 #include <dwarfs/binary_literals.h>
 
 #include <dwarfs/internal/mappable_file.h>
@@ -37,16 +42,36 @@ namespace {
 
 using namespace binary_literals;
 
+class zero_filled_mapping {
+ public:
+  zero_filled_mapping() = default;
+
+  std::shared_ptr<readonly_memory_mapping const>
+  get(memory_mapping_ops const& ops, size_t size) {
+    std::lock_guard lock{mx_};
+    if (!mapping_ || mapping_->size() < size) {
+      mapping_ = std::make_shared<readonly_memory_mapping>(
+          mappable_file::map_empty_readonly(ops, std::bit_ceil(size)));
+    }
+    return mapping_;
+  }
+
+ private:
+  std::mutex mx_;
+  std::shared_ptr<readonly_memory_mapping const> mapping_;
+};
+
 class mmap_file_view final
     : public detail::file_view_impl,
       public std::enable_shared_from_this<mmap_file_view> {
  public:
-  mmap_file_view(internal::memory_mapping_ops const& ops,
+  mmap_file_view(memory_mapping_ops const& ops,
                  std::filesystem::path const& path,
                  mmap_file_view_options const& opts)
-      : file_{internal::mappable_file::create(ops, path)}
+      : file_{mappable_file::create(ops, path)}
       , path_{path}
-      , extents_{file_.get_extents_noexcept()} {
+      , extents_{file_.get_extents_noexcept()}
+      , ops_{ops} {
     if (!opts.max_eager_map_size.has_value() ||
         file_.size() <= opts.max_eager_map_size.value()) {
       mapping_.emplace(file_.map_readonly());
@@ -91,10 +116,16 @@ class mmap_file_view final
   readonly_memory_mapping const& mapping() const { return mapping_.value(); }
 
  private:
-  internal::mappable_file file_;
+  bool range_is_all_zero(file_range range) const noexcept;
+  file_segment make_zero_filled_segment(file_range range) const;
+
+  mappable_file file_;
   std::optional<readonly_memory_mapping> mapping_;
   std::filesystem::path const path_;
   std::vector<detail::file_extent_info> const extents_;
+  std::once_flag mutable zero_filled_mapping_init_;
+  std::unique_ptr<zero_filled_mapping> mutable zero_filled_mapping_;
+  memory_mapping_ops const& ops_;
 };
 
 class mmap_ref_file_segment final : public detail::file_segment_impl {
@@ -136,7 +167,7 @@ class mmap_ref_file_segment final : public detail::file_segment_impl {
 
 class mmap_file_segment final : public detail::file_segment_impl {
  public:
-  mmap_file_segment(readonly_memory_mapping&& mm, file_range range)
+  mmap_file_segment(readonly_memory_mapping mm, file_range range)
       : mm_{std::move(mm)}
       , range_{range} {}
 
@@ -163,6 +194,66 @@ class mmap_file_segment final : public detail::file_segment_impl {
   file_range const range_;
 };
 
+class mmap_zero_file_segment final : public detail::file_segment_impl {
+ public:
+  mmap_zero_file_segment(std::shared_ptr<readonly_memory_mapping const> mm,
+                         file_range range)
+      : mm_{std::move(mm)}
+      , range_{range} {}
+
+  file_off_t offset() const noexcept override { return range_.offset(); }
+
+  file_size_t size() const noexcept override { return range_.size(); }
+
+  file_range range() const noexcept override { return range_; }
+
+  bool is_zero() const noexcept override { return true; }
+
+  std::span<std::byte const> raw_bytes() const override {
+    return mm_->const_span().subspan(0, range_.size());
+  }
+
+  void advise(io_advice /*adv*/, std::error_code& /*ec*/) const override {}
+
+  void lock(std::error_code& /*ec*/) const override {}
+
+ private:
+  std::shared_ptr<readonly_memory_mapping const> mm_;
+  file_range const range_;
+};
+
+bool mmap_file_view::range_is_all_zero(file_range range) const noexcept {
+  auto const offset = range.offset();
+  auto const size = range.size();
+  auto const end = offset + size;
+
+  assert(!extents_.empty());
+  assert(offset >= 0);
+  assert(size > 0);
+  assert(end <= file_.size());
+
+  // first, find the extent that contains the offset
+  auto const it =
+      // NOLINTNEXTLINE(modernize-use-ranges)
+      std::lower_bound(extents_.begin(), extents_.end(), offset,
+                       [](detail::file_extent_info const& ei, file_off_t off) {
+                         return ei.range.end() <= off;
+                       });
+
+  // check if the extent contains the entire range and is a hole
+  return it != extents_.end() && it->kind == extent_kind::hole &&
+         std::cmp_less_equal(end, it->range.end());
+}
+
+file_segment mmap_file_view::make_zero_filled_segment(file_range range) const {
+  std::call_once(zero_filled_mapping_init_, [this]() {
+    zero_filled_mapping_ = std::make_unique<zero_filled_mapping>();
+  });
+
+  return file_segment(std::make_shared<mmap_zero_file_segment>(
+      zero_filled_mapping_->get(ops_, range.size()), range));
+}
+
 file_segment mmap_file_view::segment_at(file_range range) const {
   auto const offset = range.offset();
   auto const size = range.size();
@@ -170,6 +261,10 @@ file_segment mmap_file_view::segment_at(file_range range) const {
   if (offset < 0 || size == 0 ||
       std::cmp_greater(offset + size, file_.size())) {
     return {};
+  }
+
+  if (range_is_all_zero(range)) {
+    return make_zero_filled_segment(range);
   }
 
   if (mapping_.has_value()) {
@@ -209,13 +304,13 @@ void mmap_file_view::copy_bytes(void* dest, file_range range,
 
 } // namespace
 
-file_view create_mmap_file_view(internal::memory_mapping_ops const& ops,
+file_view create_mmap_file_view(memory_mapping_ops const& ops,
                                 std::filesystem::path const& path,
                                 mmap_file_view_options const& opts) {
   return file_view(std::make_shared<mmap_file_view>(ops, path, opts));
 }
 
-file_view create_mmap_file_view(internal::memory_mapping_ops const& ops,
+file_view create_mmap_file_view(memory_mapping_ops const& ops,
                                 std::filesystem::path const& path) {
   return create_mmap_file_view(ops, path, {});
 }
