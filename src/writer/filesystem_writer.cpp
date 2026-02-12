4d328296ab2c9e116b80e9210dc551231b25b002
Marcus Holland-Moritz <github@mhxnet.de>
2026-02-12 20:47:55 +0100

    chore(filesystem_writer): advise compressed fsblock segments

--- src/writer/filesystem_writer.cpp
+++ src/writer/filesystem_writer.cpp
@@ -370,7 +370,18 @@ class compressed_fsblock : public fsblock::impl {
       , range_{sec.data(segment)}
       , segment_{std::move(segment)}
       , pctx_{std::move(pctx)}
-      , sec_{std::move(sec)} {}
+      , sec_{std::move(sec)} {
+    if (segment_) {
+      segment_.advise(io_advice::sequential);
+    }
+  }
+
+  ~compressed_fsblock() override {
+    if (segment_) {
+      std::error_code ec;
+      segment_.advise(io_advice::dontneed, ec);
+    }
+  }
 
   void
   compress(worker_group& wg, std::optional<std::string> /* meta */) override {
