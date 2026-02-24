d835c8a80e400243dcd9cac11a3ff971c22d5c36
Marcus Holland-Moritz <github@mhxnet.de>
2026-02-24 19:09:07 +0100

    refactor: move features from fbthrift to thrift-lite

--- cmake/libdwarfs.cmake
+++ cmake/libdwarfs.cmake
@@ -216,7 +216,7 @@ add_cpp2_thrift_library(thrift/compression.thrift
                         TARGET dwarfs_compression_thrift OUTPUT_PATH dwarfs)
 add_cpp2_thrift_library(thrift/history.thrift
                         TARGET dwarfs_history_thrift OUTPUT_PATH dwarfs)
-add_cpp2_thrift_library(thrift/features.thrift
+add_thrift_lite_library(thrift/features.thrift
                         TARGET dwarfs_features_thrift OUTPUT_PATH dwarfs)
 
 add_thrift_lite_library(test/thrift_lite_test.thrift
