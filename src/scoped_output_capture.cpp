58c286080442bf4c553fd9df0514b037eb2dd697
Marcus Holland-Moritz <github@mhxnet.de>
2026-03-29 20:20:40 +0200

    feat: add `scoped_output_capture` RAII wrapper

---
+++ src/scoped_output_capture.cpp
@@ -0,0 +1,426 @@
+/* vim:set ts=2 sw=2 sts=2 et: */
+/**
+ * \author     Marcus Holland-Moritz (github@mhxnet.de)
+ * \copyright  Copyright (c) Marcus Holland-Moritz
+ *
+ * This file is part of dwarfs.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the “Software”), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <array>
+#include <cerrno>
+#include <cstdio>
+#include <exception>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <system_error>
+#include <thread>
+#include <utility>
+
+#ifdef _WIN32
+#include <dwarfs/portability/windows.h>
+#include <fcntl.h>
+#include <io.h>
+#else
+#include <unistd.h>
+#endif
+
+#include <dwarfs/scoped_output_capture.h>
+
+#include <dwarfs/internal/scoped_output_capture_test_api.h>
+
+#ifndef _WIN32
+namespace {
+
+using posix_ops = dwarfs::internal::test::scoped_output_capture_posix_ops;
+
+int real_close(int fd) { return ::close(fd); }
+int real_dup(int fd) { return ::dup(fd); }
+int real_dup2(int from, int to) { return ::dup2(from, to); }
+int real_pipe(int fds[2]) { return ::pipe(fds); }
+ssize_t real_read(int fd, void* buf, std::size_t count) {
+  return ::read(fd, buf, count);
+}
+
+posix_ops const real_ops{
+    real_close, real_dup, real_dup2, real_pipe, real_read,
+};
+
+posix_ops const* g_ops = &real_ops;
+
+} // namespace
+
+namespace dwarfs::internal::test {
+
+scoped_output_capture_posix_ops real_scoped_output_capture_posix_ops() {
+  return real_ops;
+}
+
+scoped_output_capture_posix_ops_override::
+    scoped_output_capture_posix_ops_override(
+        scoped_output_capture_posix_ops ops)
+    : ops_{ops}
+    , previous_{g_ops} {
+  g_ops = &ops_;
+}
+
+scoped_output_capture_posix_ops_override::
+    ~scoped_output_capture_posix_ops_override() {
+  g_ops = previous_;
+}
+
+} // namespace dwarfs::internal::test
+
+#endif
+
+namespace dwarfs {
+
+namespace {
+
+using stream = scoped_output_capture::stream;
+
+#ifdef _WIN32
+int os_close(int fd) { return ::_close(fd); }
+int os_dup(int fd) { return ::_dup(fd); }
+int os_dup2(int from, int to) { return ::_dup2(from, to); }
+int os_pipe(int fds[2]) { return ::_pipe(fds, 4096, _O_BINARY); }
+int os_read(int fd, void* buf, std::size_t count) {
+  return ::_read(fd, buf, static_cast<unsigned>(count));
+}
+#else
+int os_close(int fd) { return g_ops->close_fn(fd); }
+int os_dup(int fd) { return g_ops->dup_fn(fd); }
+int os_dup2(int from, int to) { return g_ops->dup2_fn(from, to); }
+int os_pipe(int fds[2]) { return g_ops->pipe_fn(fds); }
+ssize_t os_read(int fd, void* buf, std::size_t count) {
+  return g_ops->read_fn(fd, buf, count);
+}
+#endif
+
+void flush_stream(stream which) {
+  switch (which) {
+  case stream::std_out:
+    std::fflush(stdout);
+    std::cout.flush();
+    std::wcout.flush();
+    break;
+
+  case stream::std_err:
+    std::fflush(stderr);
+    std::cerr.flush();
+    std::clog.flush();
+    std::wcerr.flush();
+    std::wclog.flush();
+    break;
+  }
+}
+
+[[noreturn]] void throw_errno(char const* what) {
+  throw std::system_error(errno, std::generic_category(), what);
+}
+
+#ifdef _WIN32
+[[noreturn]] void throw_last_error(char const* what) {
+  throw std::system_error(static_cast<int>(::GetLastError()),
+                          std::system_category(), what);
+}
+#endif
+
+int dup_fd_or_throw(int fd, char const* what) {
+  int const r = os_dup(fd);
+  if (r == -1) {
+    throw_errno(what);
+  }
+  return r;
+}
+
+void dup2_or_throw(int from, int to, char const* what) {
+  if (os_dup2(from, to) == -1) {
+    throw_errno(what);
+  }
+}
+
+void close_fd_noexcept(int& fd) noexcept {
+  if (fd != -1) {
+    os_close(fd);
+    fd = -1;
+  }
+}
+
+void close_raw_fd_noexcept(int fd) noexcept {
+  if (fd != -1) {
+    os_close(fd);
+  }
+}
+
+template <class F>
+void capture_first_exception(std::exception_ptr& ep, F&& fn) noexcept {
+  try {
+    std::forward<F>(fn)();
+  } catch (...) {
+    if (!ep) {
+      ep = std::current_exception();
+    }
+  }
+}
+
+#ifdef _WIN32
+DWORD std_handle_id_for_stream(stream which) {
+  switch (which) {
+  case stream::std_out:
+    return STD_OUTPUT_HANDLE;
+  case stream::std_err:
+    return STD_ERROR_HANDLE;
+  }
+  throw std::invalid_argument("invalid stream selection");
+}
+#endif
+
+int target_fd_for_stream(stream which) {
+  switch (which) {
+  case stream::std_out:
+#ifdef _WIN32
+    return _fileno(stdout);
+#else
+    return STDOUT_FILENO;
+#endif
+
+  case stream::std_err:
+#ifdef _WIN32
+    return _fileno(stderr);
+#else
+    return STDERR_FILENO;
+#endif
+  }
+
+  throw std::invalid_argument("invalid stream selection");
+}
+
+class capture_channel {
+ public:
+  explicit capture_channel(stream which)
+      : which_{which}
+      , target_fd_{target_fd_for_stream(which)}
+#ifdef _WIN32
+      , std_handle_id_{std_handle_id_for_stream(which)}
+#endif
+  {
+    if (target_fd_ < 0) {
+      throw std::runtime_error("target stream has no valid file descriptor");
+    }
+
+    flush_stream(which_);
+
+    saved_fd_ = dup_fd_or_throw(target_fd_, "saving original fd failed");
+
+#ifdef _WIN32
+    saved_std_handle_ = ::GetStdHandle(std_handle_id_);
+    if (saved_std_handle_ == INVALID_HANDLE_VALUE) {
+      close_fd_noexcept(saved_fd_);
+      throw_last_error("GetStdHandle failed");
+    }
+#endif
+
+    int pipe_fds[2] = {-1, -1};
+    if (os_pipe(pipe_fds) != 0) {
+      close_fd_noexcept(saved_fd_);
+      throw_errno("pipe failed");
+    }
+
+    read_fd_ = pipe_fds[0];
+    int write_fd = pipe_fds[1];
+
+    try {
+      dup2_or_throw(write_fd, target_fd_, "redirecting fd failed");
+
+#ifdef _WIN32
+      auto const redirected =
+          reinterpret_cast<HANDLE>(::_get_osfhandle(target_fd_));
+      if (redirected == INVALID_HANDLE_VALUE) {
+        throw_errno("_get_osfhandle failed");
+      }
+
+      if (!::SetStdHandle(std_handle_id_, redirected)) {
+        throw_last_error("SetStdHandle failed");
+      }
+#endif
+
+      if (os_close(write_fd) == -1) {
+        throw_errno("closing temporary pipe write fd failed");
+      }
+      write_fd = -1;
+
+      reader_ = std::thread(&capture_channel::reader_main, this);
+    } catch (...) {
+      if (write_fd != -1) {
+        os_close(write_fd);
+      }
+#ifdef _WIN32
+      if (saved_std_handle_ != INVALID_HANDLE_VALUE) {
+        ::SetStdHandle(std_handle_id_, saved_std_handle_);
+      }
+#endif
+
+      if (saved_fd_ != -1) {
+        os_dup2(saved_fd_, target_fd_);
+      }
+
+      close_fd_noexcept(saved_fd_);
+      close_fd_noexcept(read_fd_);
+      throw;
+    }
+  }
+
+  capture_channel(capture_channel const&) = delete;
+  capture_channel& operator=(capture_channel const&) = delete;
+
+  capture_channel(capture_channel&&) = delete;
+  capture_channel& operator=(capture_channel&&) = delete;
+
+  ~capture_channel() noexcept {
+    try {
+      stop();
+    } catch (...) {
+    }
+  }
+
+  void stop() {
+    if (std::exchange(stopped_, true)) {
+      return;
+    }
+
+    std::exception_ptr first_error;
+
+    flush_stream(which_);
+
+#ifdef _WIN32
+    capture_first_exception(first_error, [&] {
+      if (!::SetStdHandle(std_handle_id_, saved_std_handle_)) {
+        throw_last_error("SetStdHandle(restore) failed");
+      }
+    });
+#endif
+
+    bool restored = false;
+    capture_first_exception(first_error, [&] {
+      dup2_or_throw(saved_fd_, target_fd_, "restoring original stream failed");
+      restored = true;
+    });
+
+    if (!restored) {
+      close_raw_fd_noexcept(target_fd_);
+    }
+
+    close_fd_noexcept(saved_fd_);
+
+    reader_.join();
+
+    close_fd_noexcept(read_fd_);
+
+    if (reader_error_ && !first_error) {
+      first_error = reader_error_;
+    }
+
+    if (first_error) {
+      std::rethrow_exception(first_error);
+    }
+  }
+
+  std::string const& captured() const noexcept { return buffer_; }
+
+ private:
+  void reader_main() {
+    std::array<char, 4096> tmp{};
+
+    try {
+      for (;;) {
+        auto const n = os_read(read_fd_, tmp.data(), tmp.size());
+
+        if (n > 0) {
+          buffer_.append(tmp.data(), static_cast<std::size_t>(n));
+          continue;
+        }
+
+        if (n == 0) {
+          break;
+        }
+
+        if (errno != EINTR) {
+          throw_errno("read failed");
+        }
+      }
+    } catch (...) {
+      reader_error_ = std::current_exception();
+    }
+  }
+
+  stream which_;
+  int target_fd_ = -1;
+  int saved_fd_ = -1;
+  int read_fd_ = -1;
+  std::thread reader_;
+  std::string buffer_;
+  std::exception_ptr reader_error_;
+  bool stopped_ = false;
+
+#ifdef _WIN32
+  DWORD std_handle_id_ = 0;
+  HANDLE saved_std_handle_ = INVALID_HANDLE_VALUE;
+#endif
+};
+
+} // namespace
+
+class scoped_output_capture::impl {
+ public:
+  explicit impl(stream which)
+      : channel_{which} {}
+
+  void stop() { channel_.stop(); }
+
+  std::string const& captured() const { return channel_.captured(); }
+
+ private:
+  capture_channel channel_;
+};
+
+scoped_output_capture::scoped_output_capture(stream which)
+    : impl_(std::make_unique<impl>(which)) {}
+
+scoped_output_capture::~scoped_output_capture() = default;
+
+scoped_output_capture::scoped_output_capture(scoped_output_capture&&) noexcept =
+    default;
+
+scoped_output_capture&
+scoped_output_capture::operator=(scoped_output_capture&&) noexcept = default;
+
+void scoped_output_capture::stop() { impl_->stop(); }
+
+std::string const& scoped_output_capture::captured() {
+  stop();
+  return impl_->captured();
+}
+
+} // namespace dwarfs
---
+++ test/scoped_output_capture_test.cpp
@@ -0,0 +1,389 @@
+/* vim:set ts=2 sw=2 sts=2 et: */
+/**
+ * \author     Marcus Holland-Moritz (github@mhxnet.de)
+ * \copyright  Copyright (c) Marcus Holland-Moritz
+ *
+ * This file is part of dwarfs.
+ *
+ * dwarfs is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * dwarfs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with dwarfs.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <utility>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#ifdef _WIN32
+#include <dwarfs/portability/windows.h>
+#else
+#include <unistd.h>
+#endif
+
+#include <dwarfs/scoped_output_capture.h>
+
+#include <dwarfs/internal/scoped_output_capture_test_api.h>
+
+namespace {
+
+using ::testing::Eq;
+using ::testing::HasSubstr;
+using ::testing::ThrowsMessage;
+
+using dwarfs::scoped_output_capture;
+
+void write_stdout_fd(std::string const& text) {
+#ifdef _WIN32
+  ASSERT_NE(_fileno(stdout), -1);
+  ASSERT_NE(
+      _write(_fileno(stdout), text.data(), static_cast<unsigned>(text.size())),
+      -1);
+#else
+  ASSERT_NE(::write(STDOUT_FILENO, text.data(), text.size()), -1);
+#endif
+}
+
+void write_stderr_fd(std::string const& text) {
+#ifdef _WIN32
+  ASSERT_NE(_fileno(stderr), -1);
+  ASSERT_NE(
+      _write(_fileno(stderr), text.data(), static_cast<unsigned>(text.size())),
+      -1);
+#else
+  ASSERT_NE(::write(STDERR_FILENO, text.data(), text.size()), -1);
+#endif
+}
+
+#ifdef _WIN32
+void write_stdout_handle(std::string const& text) {
+  HANDLE handle = ::GetStdHandle(STD_OUTPUT_HANDLE);
+  ASSERT_NE(handle, INVALID_HANDLE_VALUE);
+  ASSERT_NE(handle, nullptr);
+
+  DWORD written = 0;
+  ASSERT_TRUE(::WriteFile(handle, text.data(), static_cast<DWORD>(text.size()),
+                          &written, nullptr));
+  ASSERT_EQ(written, text.size());
+}
+
+void write_stderr_handle(std::string const& text) {
+  HANDLE handle = ::GetStdHandle(STD_ERROR_HANDLE);
+  ASSERT_NE(handle, INVALID_HANDLE_VALUE);
+  ASSERT_NE(handle, nullptr);
+
+  DWORD written = 0;
+  ASSERT_TRUE(::WriteFile(handle, text.data(), static_cast<DWORD>(text.size()),
+                          &written, nullptr));
+  ASSERT_EQ(written, text.size());
+}
+#endif
+
+TEST(scoped_output_capture_test,
+     captures_stdout_via_printf_using_captured_alias) {
+  scoped_output_capture cap;
+
+  ASSERT_GE(std::printf("hello from printf"), 0);
+  std::fflush(stdout);
+
+  EXPECT_THAT(cap.captured(), Eq("hello from printf"));
+}
+
+TEST(scoped_output_capture_test, captures_stdout_via_iostream) {
+  scoped_output_capture cap;
+
+  std::cout << "hello from iostream" << std::flush;
+
+  EXPECT_THAT(cap.captured(), Eq("hello from iostream"));
+}
+
+TEST(scoped_output_capture_test, captures_stderr_via_fprintf) {
+  scoped_output_capture cap{scoped_output_capture::stream::std_err};
+
+  ASSERT_GE(std::fprintf(stderr, "%s", "hello from fprintf"), 0);
+  std::fflush(stderr);
+
+  EXPECT_THAT(cap.captured(), Eq("hello from fprintf"));
+}
+
+TEST(scoped_output_capture_test, captures_stdout_and_stderr_independently) {
+  scoped_output_capture cap_out{scoped_output_capture::stream::std_out};
+  scoped_output_capture cap_err{scoped_output_capture::stream::std_err};
+
+  ASSERT_GE(std::printf("stdout text"), 0);
+  std::fflush(stdout);
+  ASSERT_GE(std::fprintf(stderr, "%s", "stderr text"), 0);
+  std::fflush(stderr);
+
+  EXPECT_THAT(cap_out.captured(), Eq("stdout text"));
+  EXPECT_THAT(cap_err.captured(), Eq("stderr text"));
+}
+
+TEST(scoped_output_capture_test, captures_stdout_via_file_descriptor_api) {
+  scoped_output_capture cap;
+
+  write_stdout_fd("fd stdout");
+
+  EXPECT_THAT(cap.captured(), Eq("fd stdout"));
+}
+
+TEST(scoped_output_capture_test, captures_stderr_via_file_descriptor_api) {
+  scoped_output_capture cap{scoped_output_capture::stream::std_err};
+
+  write_stderr_fd("fd stderr");
+
+  EXPECT_THAT(cap.captured(), Eq("fd stderr"));
+}
+
+#ifdef _WIN32
+TEST(scoped_output_capture_test, captures_stdout_via_writefile_getstdhandle) {
+  scoped_output_capture cap;
+
+  write_stdout_handle("handle stdout");
+
+  EXPECT_THAT(cap.captured(), Eq("handle stdout"));
+}
+
+TEST(scoped_output_capture_test, captures_stderr_via_writefile_getstdhandle) {
+  scoped_output_capture cap{scoped_output_capture::stream::std_err};
+
+  write_stderr_handle("handle stderr");
+
+  EXPECT_THAT(cap.captured(), Eq("handle stderr"));
+}
+#endif
+
+TEST(scoped_output_capture_test,
+     stop_is_idempotent_and_freezes_the_first_capture) {
+  scoped_output_capture first;
+
+  write_stdout_fd("before stop");
+  ASSERT_NO_THROW(first.stop());
+  ASSERT_NO_THROW(first.stop());
+
+  scoped_output_capture second;
+  write_stdout_fd("after stop");
+
+  EXPECT_THAT(first.captured(), Eq("before stop"));
+  EXPECT_THAT(second.captured(), Eq("after stop"));
+}
+
+TEST(scoped_output_capture_test, move_constructor_preserves_active_capture) {
+  scoped_output_capture original;
+  write_stdout_fd("part one ");
+
+  scoped_output_capture moved{std::move(original)};
+  write_stdout_fd("part two");
+
+  EXPECT_THAT(moved.captured(), Eq("part one part two"));
+}
+
+TEST(scoped_output_capture_test,
+     writing_more_than_pipe_capacity_is_fully_captured) {
+  constexpr std::size_t repeats = 32 * 1024;
+  std::string expected;
+  expected.reserve(repeats);
+
+  for (std::size_t i = 0; i != repeats; ++i) {
+    expected.push_back(static_cast<char>('a' + (i % 26)));
+  }
+
+  scoped_output_capture cap;
+  write_stdout_fd(expected);
+
+  EXPECT_THAT(cap.captured(), Eq(expected));
+}
+
+TEST(scoped_output_capture_test,
+     inner_capture_does_not_tee_output_to_outer_capture) {
+  scoped_output_capture outer;
+
+  {
+    scoped_output_capture inner;
+    write_stdout_fd("inner only");
+    EXPECT_THAT(inner.captured(), Eq("inner only"));
+  }
+
+  EXPECT_THAT(outer.captured(), Eq(""));
+}
+
+TEST(scoped_output_capture_test, destructor_restores_previous_capture) {
+  scoped_output_capture outer;
+
+  {
+    scoped_output_capture inner;
+    write_stdout_fd("inner");
+  }
+
+  write_stdout_fd("outer");
+
+  EXPECT_THAT(outer.captured(), Eq("outer"));
+}
+
+#ifndef _WIN32
+
+using namespace dwarfs::internal;
+
+TEST(scoped_output_capture_test, constructor_throws_when_dup_fails) {
+  auto ops = test::real_scoped_output_capture_posix_ops();
+  ops.dup_fn = [](int) -> int {
+    errno = EMFILE;
+    return -1;
+  };
+
+  test::scoped_output_capture_posix_ops_override ops_guard{ops};
+
+  EXPECT_THROW(
+      {
+        try {
+          scoped_output_capture cap;
+        } catch (std::system_error const& e) {
+          EXPECT_EQ(e.code().value(), EMFILE);
+          EXPECT_THAT(e.what(), HasSubstr("saving original fd failed"));
+          throw;
+        }
+      },
+      std::system_error);
+}
+
+TEST(scoped_output_capture_test, constructor_throws_when_pipe_fails) {
+  auto ops = test::real_scoped_output_capture_posix_ops();
+  ops.pipe_fn = [](int[2]) -> int {
+    errno = ENFILE;
+    return -1;
+  };
+
+  test::scoped_output_capture_posix_ops_override ops_guard{ops};
+
+  EXPECT_THROW(
+      {
+        try {
+          scoped_output_capture cap;
+        } catch (std::system_error const& e) {
+          EXPECT_EQ(e.code().value(), ENFILE);
+          EXPECT_THAT(e.what(), HasSubstr("pipe failed"));
+          throw;
+        }
+      },
+      std::system_error);
+}
+
+TEST(scoped_output_capture_test,
+     constructor_rolls_back_when_redirect_dup2_fails) {
+  int dup2_calls = 0;
+
+  auto ops = test::real_scoped_output_capture_posix_ops();
+  ops.dup2_fn = [&](int, int) -> int {
+    ++dup2_calls;
+    errno = EBADF;
+    return -1;
+  };
+
+  {
+    test::scoped_output_capture_posix_ops_override ops_guard{ops};
+    EXPECT_THROW(scoped_output_capture{}, std::system_error);
+  }
+
+  EXPECT_EQ(2, dup2_calls) << "dup2 should have been called twice";
+
+  // After rollback, a normal capture should still work.
+  {
+    scoped_output_capture cap;
+    std::fputs("ok", stdout);
+    EXPECT_EQ(cap.captured(), "ok");
+  }
+}
+
+TEST(scoped_output_capture_test, stop_rethrows_reader_error) {
+  auto ops = test::real_scoped_output_capture_posix_ops();
+  ops.read_fn = [](int, void*, std::size_t) -> ssize_t {
+    errno = EIO;
+    return -1;
+  };
+
+  test::scoped_output_capture_posix_ops_override ops_guard{ops};
+
+  scoped_output_capture cap;
+  std::fputs("x", stdout);
+
+  EXPECT_THROW(
+      {
+        try {
+          cap.stop();
+        } catch (std::system_error const& e) {
+          EXPECT_EQ(e.code().value(), EIO);
+          EXPECT_THAT(e.what(), HasSubstr("read failed"));
+          throw;
+        }
+      },
+      std::system_error);
+}
+
+TEST(scoped_output_capture_test, reader_retries_after_eintr) {
+  std::atomic<bool> first_read = true;
+
+  auto ops = test::real_scoped_output_capture_posix_ops();
+  ops.read_fn = [&](int fd, void* buf, std::size_t count) -> ssize_t {
+    if (first_read.exchange(false)) {
+      errno = EINTR;
+      return -1;
+    }
+    return ::read(fd, buf, count);
+  };
+
+  test::scoped_output_capture_posix_ops_override ops_guard{ops};
+
+  scoped_output_capture cap;
+  std::fputs("hello", stdout);
+
+  EXPECT_EQ(cap.captured(), "hello");
+}
+
+TEST(scoped_output_capture_test, stop_throws_when_restore_dup2_fails) {
+  int dup2_call_count = 0;
+
+  auto ops = test::real_scoped_output_capture_posix_ops();
+  ops.dup2_fn = [&](int from, int to) -> int {
+    ++dup2_call_count;
+    if (dup2_call_count == 2) {
+      errno = EIO;
+      return -1;
+    }
+    return ::dup2(from, to);
+  };
+
+  test::scoped_output_capture_posix_ops_override ops_guard{ops};
+
+  scoped_output_capture cap;
+  std::fputs("hello", stdout);
+
+  EXPECT_THROW(
+      {
+        try {
+          cap.stop();
+        } catch (std::system_error const& e) {
+          EXPECT_EQ(e.code().value(), EIO);
+          EXPECT_THAT(e.what(), HasSubstr("restoring original stream failed"));
+          throw;
+        }
+      },
+      std::system_error);
+}
+
+#endif
+
+} // namespace
