228567e28e8afc70bf68519fb4d792e64f5fdbbc
Marcus Holland-Moritz <github@mhxnet.de>
2026-04-05 16:06:22 +0200

    refactor: switch entry handles from internal pointers to `entry_id`

--- src/writer/entry_handle.cpp
+++ src/writer/entry_handle.cpp
@@ -24,6 +24,7 @@
 #include <fmt/format.h>
 
 #include <dwarfs/writer/entry_handle.h>
+#include <dwarfs/writer/entry_storage.h>
 
 #include <dwarfs/writer/internal/entry.h>
 
@@ -31,87 +32,92 @@ namespace dwarfs::writer {
 
 namespace detail {
 
+template <detail::mutability Mut>
+auto entry_handle_base<Mut>::base() const -> base_t* {
+  return static_cast<base_t*>(storage_->get_entry(self_id_));
+}
+
 template <mutability Mut>
 bool entry_handle_base<Mut>::has_parent() const {
-  return self_->has_parent();
+  return base()->has_parent();
 }
 
 template <mutability Mut>
 basic_entry_handle<Mut> entry_handle_base<Mut>::parent() const {
-  return {*storage_, self_->parent()};
+  return {*storage_, base()->parent_id()};
 }
 
 template <mutability Mut>
 std::filesystem::path entry_handle_base<Mut>::fs_path() const {
-  return self_->fs_path();
+  return base()->fs_path();
 }
 
 template <mutability Mut>
 std::string entry_handle_base<Mut>::path_as_string() const {
-  return self_->path_as_string();
+  return base()->path_as_string();
 }
 
 template <mutability Mut>
 std::string entry_handle_base<Mut>::unix_dpath() const {
-  return self_->unix_dpath();
+  return base()->unix_dpath();
 }
 
 template <mutability Mut>
 std::string_view entry_handle_base<Mut>::name() const {
-  return self_->name();
+  return base()->name();
 }
 
 template <mutability Mut>
 bool entry_handle_base<Mut>::less_revpath(const_entry_handle rhs) const {
-  return self_->less_revpath(*rhs.self_);
+  return base()->less_revpath(*rhs.base());
 }
 
 template <mutability Mut>
 file_size_t entry_handle_base<Mut>::size() const {
-  return self_->size();
+  return base()->size();
 }
 
 template <mutability Mut>
 file_size_t entry_handle_base<Mut>::allocated_size() const {
-  return self_->allocated_size();
+  return base()->allocated_size();
 }
 
 template <mutability Mut>
 entry_type entry_handle_base<Mut>::type() const {
-  return self_->type();
+  return base()->type();
 }
 
 template <mutability Mut>
 void entry_handle_base<Mut>::update(internal::global_entry_data& data) const {
-  self_->update(data);
+  base()->update(data);
 }
 
 template <mutability Mut>
 unique_inode_id entry_handle_base<Mut>::inode_id() const {
-  return self_->inode_id();
+  return base()->inode_id();
 }
 
 template <mutability Mut>
 uint64_t entry_handle_base<Mut>::num_hard_links() const {
-  return self_->num_hard_links();
+  return base()->num_hard_links();
 }
 
 template <mutability Mut>
 std::optional<uint32_t> const& entry_handle_base<Mut>::inode_num() const {
-  return self_->inode_num(*storage_);
+  return base()->inode_num(*storage_);
 }
 
 template <mutability Mut>
 void entry_handle_base<Mut>::accept(entry_handle_visitor& v, bool preorder)
   requires is_mutable
 {
-  switch (self_->type()) {
+  switch (base()->type()) {
   case entry_type::E_FILE:
-    v.visit(file_handle{*storage_, self_->as_file()});
+    v.visit(file_handle{*storage_, self_id_});
     break;
 
   case entry_type::E_DIR: {
-    auto dir = dir_handle{*storage_, self_->as_dir()};
+    auto dir = dir_handle{*storage_, self_id_};
 
     if (preorder) {
       v.visit(dir);
@@ -125,12 +131,12 @@ void entry_handle_base<Mut>::accept(entry_handle_visitor& v, bool preorder)
   } break;
 
   case entry_type::E_LINK:
-    v.visit(link_handle{*storage_, self_->as_link()});
+    v.visit(link_handle{*storage_, self_id_});
     break;
 
   case entry_type::E_DEVICE:
   case entry_type::E_OTHER:
-    v.visit(device_handle{*storage_, self_->as_device()});
+    v.visit(device_handle{*storage_, self_id_});
     break;
   }
 }
@@ -139,40 +145,48 @@ template <mutability Mut>
 void entry_handle_base<Mut>::scan(os_access const& os, internal::progress& prog)
   requires is_mutable
 {
-  self_->scan(os, prog);
+  base()->scan(os, prog);
 }
 
 template <mutability Mut>
 void entry_handle_base<Mut>::set_empty()
   requires is_mutable
 {
-  self_->set_empty();
+  base()->set_empty();
 }
 
 template <mutability Mut>
 void entry_handle_base<Mut>::set_entry_index(uint32_t index)
   requires is_mutable
 {
-  self_->set_entry_index(index);
+  base()->set_entry_index(index);
 }
 
 template <mutability Mut>
 void entry_handle_base<Mut>::set_inode_num(uint32_t ino)
   requires is_mutable
 {
-  self_->set_inode_num(*storage_, ino);
+  base()->set_inode_num(*storage_, ino);
 }
 
 template <mutability Mut>
 void entry_handle_base<Mut>::walk(std::function<void(entry_handle)> const& f)
   requires is_mutable
 {
-  f(entry_handle{*storage_, self_});
+  f(entry_handle{*storage_, self_id_});
 
-  if (self_->is_dir()) {
-    dir_handle{*storage_, self_->as_dir()}.for_each_child(
+  if (base()->is_dir()) {
+    dir_handle{*storage_, self_id_}.for_each_child(
         [&](entry_handle child) { child.walk(f); });
   }
+}
+
+template <mutability Mut>
+void entry_handle_base<Mut>::pack(
+    thrift::metadata::inode_data& entry_v2,
+    internal::global_entry_data const& data,
+    internal::time_resolution_converter const& timeres) const {
+  base()->pack(entry_v2, data, timeres);
 }
 
 template class entry_handle_base<mutability::const_>;
@@ -184,19 +198,19 @@ template class entry_handle_base<mutability::mutable_>;
 
 template <detail::mutability Mut>
 auto basic_file_handle<Mut>::self() const -> self_t* {
-  return static_cast<self_t*>(this->self_);
+  return static_cast<self_t*>(this->base());
 }
 
 template <detail::mutability Mut>
 std::string_view basic_file_handle<Mut>::hash() const {
-  return self()->hash(*this->storage_);
+  return self()->hash(this->storage());
 }
 
 template <detail::mutability Mut>
 void basic_file_handle<Mut>::create_data()
   requires is_mutable
 {
-  self()->create_data(*this->storage_);
+  self()->create_data(this->storage());
 }
 
 template <detail::mutability Mut>
@@ -204,19 +218,19 @@ void basic_file_handle<Mut>::scan(file_view const& mm, internal::progress& prog,
                                   std::optional<std::string> const& hash_alg)
   requires is_mutable
 {
-  self()->scan(*this->storage_, mm, prog, hash_alg);
+  self()->scan(this->storage(), mm, prog, hash_alg);
 }
 
 template <detail::mutability Mut>
 void basic_file_handle<Mut>::set_invalid()
   requires is_mutable
 {
-  self()->set_invalid(*this->storage_);
+  self()->set_invalid(this->storage());
 }
 
 template <detail::mutability Mut>
 bool basic_file_handle<Mut>::is_invalid() const {
-  return self()->is_invalid(*this->storage_);
+  return self()->is_invalid(this->storage());
 }
 
 template <detail::mutability Mut>
@@ -236,12 +250,12 @@ void basic_file_handle<Mut>::hardlink(file_handle other,
                                       internal::progress& prog)
   requires is_mutable
 {
-  self()->hardlink(*this->storage_, other.self(), prog);
+  self()->hardlink(this->storage(), other.self(), prog);
 }
 
 template <detail::mutability Mut>
 uint32_t basic_file_handle<Mut>::hardlink_count() const {
-  return self()->hardlink_count(*this->storage_);
+  return self()->hardlink_count(this->storage());
 }
 
 template <detail::mutability Mut>
@@ -270,49 +284,54 @@ std::string basic_file_handle<Mut>::ptr_as_string() const {
 
 template <detail::mutability Mut>
 auto basic_dir_handle<Mut>::self() const -> self_t* {
-  return static_cast<self_t*>(this->self_);
+  return static_cast<self_t*>(this->base());
 }
 
 template <detail::mutability Mut>
 void basic_dir_handle<Mut>::add(entry_handle h)
   requires is_mutable
 {
-  self()->add(h.self_);
+  self()->add(h);
 }
 
 template <detail::mutability Mut>
 void basic_dir_handle<Mut>::sort()
   requires is_mutable
 {
-  self()->sort();
+  self()->sort(this->storage());
 }
 
 template <detail::mutability Mut>
 void basic_dir_handle<Mut>::remove_empty_dirs(internal::progress& prog)
   requires is_mutable
 {
-  self()->remove_empty_dirs(prog);
+  self()->remove_empty_dirs(this->storage(), prog);
 }
 
 template <detail::mutability Mut>
 entry_handle basic_dir_handle<Mut>::find(std::filesystem::path const& path)
   requires is_mutable
 {
-  return {*this->storage_, self()->find(path)};
+  return {this->storage(), self()->find(this->storage(), path)};
+}
+
+template <detail::mutability Mut>
+bool basic_dir_handle<Mut>::empty() const {
+  return self()->empty();
 }
 
 template <detail::mutability Mut>
 void basic_dir_handle<Mut>::pack(
     thrift::metadata::metadata& mv2, internal::global_entry_data const& data,
     internal::time_resolution_converter const& timeres) const {
-  self()->pack(*this->storage_, mv2, data, timeres);
+  self()->pack(this->storage(), mv2, data, timeres);
 }
 
 template <detail::mutability Mut>
 void basic_dir_handle<Mut>::pack_entry(
     thrift::metadata::metadata& mv2, internal::global_entry_data const& data,
     internal::time_resolution_converter const& timeres) const {
-  self()->pack_entry(*this->storage_, mv2, data, timeres);
+  self()->pack_entry(this->storage(), mv2, data, timeres);
 }
 
 template <detail::mutability Mut>
@@ -320,14 +339,14 @@ void basic_dir_handle<Mut>::for_each_child(
     std::function<void(entry_handle)> const& f)
   requires is_mutable
 {
-  self()->for_each_child([&](internal::entry* e) { f({*this->storage_, e}); });
+  self()->for_each_child([&](entry_id id) { f({this->storage(), id}); });
 }
 
 // ---------- link_handle ----------
 
 template <detail::mutability Mut>
 auto basic_link_handle<Mut>::self() const -> self_t* {
-  return static_cast<self_t*>(this->self_);
+  return static_cast<self_t*>(this->base());
 }
 
 template <detail::mutability Mut>
@@ -339,7 +358,7 @@ std::string const& basic_link_handle<Mut>::linkname() const {
 
 template <detail::mutability Mut>
 auto basic_device_handle<Mut>::self() const -> self_t* {
-  return static_cast<self_t*>(this->self_);
+  return static_cast<self_t*>(this->base());
 }
 
 template <detail::mutability Mut>
@@ -353,6 +372,11 @@ std::uint64_t basic_device_handle<Mut>::device_id() const {
 }
 
 // ---------- entry_handle ----------
+
+template <detail::mutability Mut>
+auto basic_entry_handle<Mut>::self() const -> self_t* {
+  return static_cast<self_t*>(this->base());
+}
 
 template <detail::mutability Mut>
 basic_entry_handle<Mut>::basic_entry_handle(basic_file_handle<Mut> h)
@@ -397,47 +421,59 @@ basic_entry_handle<Mut>::basic_entry_handle(device_handle h)
 
 template <detail::mutability Mut>
 bool basic_entry_handle<Mut>::is_file() const noexcept {
-  return this->self_->is_file();
+  return this->self()->is_file();
 }
 
 template <detail::mutability Mut>
 bool basic_entry_handle<Mut>::is_dir() const noexcept {
-  return this->self_->is_dir();
+  return this->self()->is_dir();
 }
 
 template <detail::mutability Mut>
 bool basic_entry_handle<Mut>::is_link() const noexcept {
-  return this->self_->is_link();
+  return this->self()->is_link();
 }
 
 template <detail::mutability Mut>
 bool basic_entry_handle<Mut>::is_device() const noexcept {
-  return this->self_->is_device();
+  return this->self()->is_device();
 }
 
 template <detail::mutability Mut>
 bool basic_entry_handle<Mut>::is_other() const noexcept {
-  return this->self_->is_other();
+  return this->self()->is_other();
 }
 
 template <detail::mutability Mut>
 basic_file_handle<Mut> basic_entry_handle<Mut>::as_file() const noexcept {
-  return {*this->storage_, this->self_->as_file()};
+  if (is_file()) {
+    return this->template base_as<basic_file_handle<Mut>>();
+  }
+  return {};
 }
 
 template <detail::mutability Mut>
 basic_dir_handle<Mut> basic_entry_handle<Mut>::as_dir() const noexcept {
-  return {*this->storage_, this->self_->as_dir()};
+  if (is_dir()) {
+    return this->template base_as<basic_dir_handle<Mut>>();
+  }
+  return {};
 }
 
 template <detail::mutability Mut>
 basic_link_handle<Mut> basic_entry_handle<Mut>::as_link() const noexcept {
-  return {*this->storage_, this->self_->as_link()};
+  if (is_link()) {
+    return this->template base_as<basic_link_handle<Mut>>();
+  }
+  return {};
 }
 
 template <detail::mutability Mut>
 basic_device_handle<Mut> basic_entry_handle<Mut>::as_device() const noexcept {
-  return {*this->storage_, this->self_->as_device()};
+  if (is_device() || is_other()) {
+    return this->template base_as<basic_device_handle<Mut>>();
+  }
+  return {};
 }
 
 // explicit instantiations
