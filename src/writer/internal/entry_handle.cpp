b8591380dddbb0b18fe4b80e53cfa1ed5f645c9e
Marcus Holland-Moritz <github@mhxnet.de>
2026-04-13 22:00:33 +0200

    refactor: rename `device_id()` to `posix_device_id()`

--- src/writer/internal/entry_handle.cpp
+++ src/writer/internal/entry_handle.cpp
@@ -403,7 +403,7 @@ auto basic_device_handle<Mut>::self() const -> self_t* {
 }
 
 template <detail::mutability Mut>
-std::uint64_t basic_device_handle<Mut>::device_id() const {
+std::uint64_t basic_device_handle<Mut>::posix_device_id() const {
   return self()->device_id();
 }
 
