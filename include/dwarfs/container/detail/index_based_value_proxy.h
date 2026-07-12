fcd86dd2ce8196859ed5955f20229e4efd69ceaa
Marcus Holland-Moritz <github@mhxnet.de>
2026-07-12 13:05:08 +0200

    refactor(container): use policy based design for index-based proxies

--- include/dwarfs/container/detail/index_based_value_proxy.h
+++ include/dwarfs/container/detail/index_based_value_proxy.h
@@ -31,9 +31,11 @@
 #include <compare>
 #include <concepts>
 #include <cstddef>
+#include <functional>
 #include <tuple>
 #include <type_traits>
 
+#include <dwarfs/container/detail/concepts.h>
 #include <dwarfs/container/detail/packed_field_descriptor.h>
 
 namespace dwarfs::container::detail {
@@ -48,382 +50,230 @@ template <typename T>
 constexpr inline bool is_std_optional_v = is_std_optional<T>::value;
 
 template <typename Container, std::size_t I>
-class index_based_field_proxy {
- public:
+struct proxy_field_accessor_policy {
   using container_type = Container;
-  using size_type = typename container_type::size_type;
+  using size_type = typename Container::size_type;
   using value_type = typename packed_field_descriptor<
-      typename container_type::value_type>::template field_value_type<I>;
+      typename Container::value_type>::template field_value_type<I>;
 
-  index_based_field_proxy(container_type& vec, size_type i) noexcept
+  // A single field is a leaf: it has no sub-fields of its own.
+  static constexpr size_type subfield_count = 1;
+
+  static value_type load(container_type& c, size_type i) {
+    return c.template get_field<I>(i);
+  }
+  static void store(container_type& c, size_type i, value_type value) {
+    c.template set_field<I>(i, value);
+  }
+};
+
+template <typename Container>
+struct proxy_value_accessor_policy {
+  using container_type = Container;
+  using size_type = typename Container::size_type;
+  using value_type = typename Container::value_type;
+
+  static constexpr size_type subfield_count =
+      packed_field_descriptor<value_type>::field_count;
+
+  template <size_type I>
+  using subfield_accessor = proxy_field_accessor_policy<Container, I>;
+
+  static value_type load(container_type& c, size_type i) { return c.get(i); }
+  static void store(container_type& c, size_type i, value_type value) {
+    c.set(i, value);
+  }
+};
+
+template <typename AccessorPolicy>
+class basic_index_based_proxy {
+ public:
+  using container_type = typename AccessorPolicy::container_type;
+  using size_type = typename AccessorPolicy::size_type;
+  using value_type = typename AccessorPolicy::value_type;
+
+  basic_index_based_proxy(container_type& vec, size_type i) noexcept
       : vec_{vec}
       , i_{i} {}
 
-  index_based_field_proxy(index_based_field_proxy const&) noexcept = default;
-  index_based_field_proxy(index_based_field_proxy&&) noexcept = default;
+  basic_index_based_proxy(basic_index_based_proxy const&) noexcept = default;
+  basic_index_based_proxy(basic_index_based_proxy&&) noexcept = default;
 
-  operator value_type() const { return vec_.template get_field<I>(i_); }
+  operator value_type() const { return AccessorPolicy::load(vec_, i_); }
   [[nodiscard]] auto load() const -> value_type {
-    return static_cast<value_type>(*this);
+    return AccessorPolicy::load(vec_, i_);
   }
   [[nodiscard]] auto operator+() const -> value_type { return load(); }
 
-  index_based_field_proxy& operator=(value_type value) {
-    vec_.template set_field<I>(i_, value);
+  basic_index_based_proxy& operator=(value_type value) {
+    AccessorPolicy::store(vec_, i_, value);
     return *this;
   }
 
   // Required for proxy-reference style semantics.
   // NOLINTNEXTLINE(misc-unconventional-assign-operator,cppcoreguidelines-c-copy-assignment-signature)
-  index_based_field_proxy const& operator=(value_type value) const {
-    vec_.template set_field<I>(i_, value);
+  basic_index_based_proxy const& operator=(value_type value) const {
+    AccessorPolicy::store(vec_, i_, value);
     return *this;
   }
 
-  index_based_field_proxy& operator=(index_based_field_proxy const& other) {
+  basic_index_based_proxy& operator=(basic_index_based_proxy const& other) {
     if (this != &other) {
-      *this = static_cast<value_type>(other);
+      *this = other.load();
     }
     return *this;
   }
 
   friend void
-  swap(index_based_field_proxy a, index_based_field_proxy b) noexcept {
-    value_type tmp = a;
-    a = static_cast<value_type>(b);
+  swap(basic_index_based_proxy a, basic_index_based_proxy b) noexcept {
+    value_type tmp = a.load();
+    a = b.load();
     b = tmp;
   }
 
-  // arithmetic operators support
-
-  template <typename Rhs>
-  index_based_field_proxy& operator+=(Rhs&& rhs)
-    requires requires(value_type lhs, Rhs&& r) {
-      { lhs + std::forward<Rhs>(r) } -> std::convertible_to<value_type>;
-    }
-  {
-    *this = static_cast<value_type>(*this) + std::forward<Rhs>(rhs);
-    return *this;
-  }
-
-  template <typename Rhs>
-  index_based_field_proxy& operator-=(Rhs&& rhs)
-    requires requires(value_type lhs, Rhs&& r) {
-      { lhs - std::forward<Rhs>(r) } -> std::convertible_to<value_type>;
-    }
-  {
-    *this = static_cast<value_type>(*this) - std::forward<Rhs>(rhs);
-    return *this;
-  }
-
-  index_based_field_proxy& operator++()
-    requires requires(value_type lhs) {
-      { lhs + 1 } -> std::convertible_to<value_type>;
-    }
-  {
-    *this += 1;
-    return *this;
-  }
-
-  value_type operator++(int)
-    requires requires(value_type lhs) {
-      { lhs + 1 } -> std::convertible_to<value_type>;
-    }
-  {
-    value_type old = *this;
-    ++*this;
-    return old;
-  }
-
-  index_based_field_proxy& operator--()
-    requires requires(value_type lhs) {
-      { lhs - 1 } -> std::convertible_to<value_type>;
-    }
-  {
-    *this -= 1;
-    return *this;
-  }
-
-  value_type operator--(int)
-    requires requires(value_type lhs) {
-      { lhs - 1 } -> std::convertible_to<value_type>;
-    }
-  {
-    value_type old = *this;
-    --*this;
-    return old;
-  }
-
-  // optional-like interface for optional fields
-
-  bool has_value() const
-    requires is_std_optional_v<value_type>
-  {
-    return static_cast<value_type>(*this).has_value();
-  }
-
-  auto value() const
-    requires is_std_optional_v<value_type>
-  {
-    return static_cast<value_type>(*this).value();
-  }
-
-  auto operator*() const
-    requires is_std_optional_v<value_type>
-  {
-    return value();
-  }
-
-  friend bool
-  operator==(index_based_field_proxy const& lhs, value_type const& rhs)
-    requires std::is_class_v<value_type>
-  {
-    return static_cast<value_type>(lhs) == rhs;
-  }
-
-  friend bool
-  operator==(value_type const& lhs, index_based_field_proxy const& rhs)
-    requires std::is_class_v<value_type>
-  {
-    return lhs == static_cast<value_type>(rhs);
-  }
-
-  friend bool operator==(index_based_field_proxy const& lhs,
-                         index_based_field_proxy const& rhs)
-    requires std::is_class_v<value_type>
-  {
-    return static_cast<value_type>(lhs) == static_cast<value_type>(rhs);
-  }
-
-  friend auto
-  operator<=>(index_based_field_proxy const& lhs, value_type const& rhs)
-      -> std::compare_three_way_result_t<value_type>
-    requires std::is_class_v<value_type> &&
-             std::three_way_comparable<value_type>
-  {
-    return static_cast<value_type>(lhs) <=> rhs;
-  }
-
-  friend auto
-  operator<=>(value_type const& lhs, index_based_field_proxy const& rhs)
-      -> std::compare_three_way_result_t<value_type>
-    requires std::is_class_v<value_type> &&
-             std::three_way_comparable<value_type>
-  {
-    return lhs <=> static_cast<value_type>(rhs);
-  }
-
-  friend auto operator<=>(index_based_field_proxy const& lhs,
-                          index_based_field_proxy const& rhs)
-      -> std::compare_three_way_result_t<value_type>
-    requires std::is_class_v<value_type> &&
-             std::three_way_comparable<value_type>
-  {
-    return static_cast<value_type>(lhs) <=> static_cast<value_type>(rhs);
-  }
-
- private:
-  container_type& vec_;
-  size_type i_;
-};
-
-template <typename Container>
-class index_based_value_proxy {
- public:
-  using container_type = Container;
-  using value_type = typename container_type::value_type;
-  using size_type = typename container_type::size_type;
-
- private:
-  using field_descriptor = packed_field_descriptor<value_type>;
-  static constexpr size_type field_count = field_descriptor::field_count;
-
- public:
-  index_based_value_proxy(container_type& vec, size_type i) noexcept
-      : vec_{vec}
-      , i_{i} {}
-
-  index_based_value_proxy(index_based_value_proxy const&) noexcept = default;
-  index_based_value_proxy(index_based_value_proxy&&) noexcept = default;
-
-  operator value_type() const { return vec_.get(i_); }
-  [[nodiscard]] auto load() const -> value_type {
-    return static_cast<value_type>(*this);
-  }
-  [[nodiscard]] auto operator+() const -> value_type { return load(); }
-
-  index_based_value_proxy& operator=(value_type value) {
-    vec_.set(i_, value);
-    return *this;
-  }
-
-  // Required for proxy-reference iterators to satisfy indirectly_writable.
-  // NOLINTNEXTLINE(misc-unconventional-assign-operator,cppcoreguidelines-c-copy-assignment-signature)
-  index_based_value_proxy const& operator=(value_type value) const {
-    vec_.set(i_, value);
-    return *this;
-  }
-
-  index_based_value_proxy& operator=(index_based_value_proxy const& other) {
-    if (this != &other) {
-      *this = static_cast<value_type>(other);
-    }
-    return *this;
-  }
-
-  friend void
-  swap(index_based_value_proxy a, index_based_value_proxy b) noexcept {
-    value_type tmp = a;
-    a = static_cast<value_type>(b);
-    b = tmp;
-  }
+  // tuple-like access (only for proxies that actually have sub-fields)
 
   template <size_type I>
   [[nodiscard]] auto get() const
-    requires(field_count > 1)
+    requires(AccessorPolicy::subfield_count > 1)
   {
-    static_assert(I < field_count);
-    return index_based_field_proxy<container_type, I>{vec_, i_};
-  }
-
-  // arithmetic operators support
-
-  template <typename Rhs>
-  index_based_value_proxy& operator+=(Rhs&& rhs)
-    requires requires(value_type lhs, Rhs&& r) {
-      { lhs + std::forward<Rhs>(r) } -> std::convertible_to<value_type>;
-    }
-  {
-    *this = static_cast<value_type>(*this) + std::forward<Rhs>(rhs);
-    return *this;
-  }
-
-  template <typename Rhs>
-  index_based_value_proxy& operator-=(Rhs&& rhs)
-    requires requires(value_type lhs, Rhs&& r) {
-      { lhs - std::forward<Rhs>(r) } -> std::convertible_to<value_type>;
-    }
-  {
-    *this = static_cast<value_type>(*this) - std::forward<Rhs>(rhs);
-    return *this;
-  }
-
-  index_based_value_proxy& operator++()
-    requires requires(value_type lhs) {
-      { lhs + 1 } -> std::convertible_to<value_type>;
-    }
-  {
-    *this += 1;
-    return *this;
-  }
-
-  value_type operator++(int)
-    requires requires(value_type lhs) {
-      { lhs + 1 } -> std::convertible_to<value_type>;
-    }
-  {
-    value_type old = *this;
-    ++*this;
-    return old;
-  }
-
-  index_based_value_proxy& operator--()
-    requires requires(value_type lhs) {
-      { lhs - 1 } -> std::convertible_to<value_type>;
-    }
-  {
-    *this -= 1;
-    return *this;
-  }
-
-  value_type operator--(int)
-    requires requires(value_type lhs) {
-      { lhs - 1 } -> std::convertible_to<value_type>;
-    }
-  {
-    value_type old = *this;
-    --*this;
-    return old;
-  }
-
-  // optional-like interface for optional fields
-
-  bool has_value() const
-    requires is_std_optional_v<value_type>
-  {
-    return static_cast<value_type>(*this).has_value();
-  }
-
-  auto value() const
-    requires is_std_optional_v<value_type>
-  {
-    return static_cast<value_type>(*this).value();
-  }
-
-  auto operator*() const
-    requires is_std_optional_v<value_type>
-  {
-    return value();
-  }
-
-  friend bool
-  operator==(index_based_value_proxy const& lhs, value_type const& rhs)
-    requires std::is_class_v<value_type>
-  {
-    return static_cast<value_type>(lhs) == rhs;
-  }
-
-  friend bool
-  operator==(value_type const& lhs, index_based_value_proxy const& rhs)
-    requires std::is_class_v<value_type>
-  {
-    return lhs == static_cast<value_type>(rhs);
-  }
-
-  friend bool operator==(index_based_value_proxy const& lhs,
-                         index_based_value_proxy const& rhs)
-    requires std::is_class_v<value_type>
-  {
-    return static_cast<value_type>(lhs) == static_cast<value_type>(rhs);
-  }
-
-  friend auto
-  operator<=>(index_based_value_proxy const& lhs, value_type const& rhs)
-      -> std::compare_three_way_result_t<value_type>
-    requires std::is_class_v<value_type> &&
-             std::three_way_comparable<value_type>
-  {
-    return static_cast<value_type>(lhs) <=> rhs;
-  }
-
-  friend auto
-  operator<=>(value_type const& lhs, index_based_value_proxy const& rhs)
-      -> std::compare_three_way_result_t<value_type>
-    requires std::is_class_v<value_type> &&
-             std::three_way_comparable<value_type>
-  {
-    return lhs <=> static_cast<value_type>(rhs);
-  }
-
-  friend auto operator<=>(index_based_value_proxy const& lhs,
-                          index_based_value_proxy const& rhs)
-      -> std::compare_three_way_result_t<value_type>
-    requires std::is_class_v<value_type> &&
-             std::three_way_comparable<value_type>
-  {
-    return static_cast<value_type>(lhs) <=> static_cast<value_type>(rhs);
+    static_assert(I < AccessorPolicy::subfield_count);
+    return basic_index_based_proxy<
+        typename AccessorPolicy::template subfield_accessor<I>>{vec_, i_};
   }
 
   template <size_type I>
-  friend auto get(index_based_value_proxy const& proxy)
-    requires(field_count > 1)
+  friend auto get(basic_index_based_proxy const& proxy)
+    requires(AccessorPolicy::subfield_count > 1)
   {
     return proxy.template get<I>();
   }
 
+  // arithmetic operators support
+
+  template <typename Rhs>
+    requires closed_under<std::plus<>, value_type, Rhs>
+  basic_index_based_proxy& operator+=(Rhs&& rhs) {
+    *this = std::plus<>{}(load(), std::forward<Rhs>(rhs));
+    return *this;
+  }
+
+  template <typename Rhs>
+    requires closed_under<std::minus<>, value_type, Rhs>
+  basic_index_based_proxy& operator-=(Rhs&& rhs) {
+    *this = std::minus<>{}(load(), std::forward<Rhs>(rhs));
+    return *this;
+  }
+
+  basic_index_based_proxy& operator++()
+    requires closed_under<std::plus<>, value_type, int>
+  {
+    *this += 1;
+    return *this;
+  }
+
+  value_type operator++(int)
+    requires closed_under<std::plus<>, value_type, int>
+  {
+    value_type old = load();
+    ++*this;
+    return old;
+  }
+
+  basic_index_based_proxy& operator--()
+    requires closed_under<std::minus<>, value_type, int>
+  {
+    *this -= 1;
+    return *this;
+  }
+
+  value_type operator--(int)
+    requires closed_under<std::minus<>, value_type, int>
+  {
+    value_type old = load();
+    --*this;
+    return old;
+  }
+
+  // optional-like interface for optional fields
+
+  bool has_value() const
+    requires is_std_optional_v<value_type>
+  {
+    return static_cast<value_type>(*this).has_value();
+  }
+
+  auto value() const
+    requires is_std_optional_v<value_type>
+  {
+    return static_cast<value_type>(*this).value();
+  }
+
+  auto operator*() const
+    requires is_std_optional_v<value_type>
+  {
+    return value();
+  }
+
+  friend bool
+  operator==(basic_index_based_proxy const& lhs, value_type const& rhs)
+    requires std::is_class_v<value_type>
+  {
+    return static_cast<value_type>(lhs) == rhs;
+  }
+
+  friend bool
+  operator==(value_type const& lhs, basic_index_based_proxy const& rhs)
+    requires std::is_class_v<value_type>
+  {
+    return lhs == static_cast<value_type>(rhs);
+  }
+
+  friend bool operator==(basic_index_based_proxy const& lhs,
+                         basic_index_based_proxy const& rhs)
+    requires std::is_class_v<value_type>
+  {
+    return static_cast<value_type>(lhs) == static_cast<value_type>(rhs);
+  }
+
+  friend auto
+  operator<=>(basic_index_based_proxy const& lhs, value_type const& rhs)
+      -> std::compare_three_way_result_t<value_type>
+    requires std::is_class_v<value_type> &&
+             std::three_way_comparable<value_type>
+  {
+    return static_cast<value_type>(lhs) <=> rhs;
+  }
+
+  friend auto
+  operator<=>(value_type const& lhs, basic_index_based_proxy const& rhs)
+      -> std::compare_three_way_result_t<value_type>
+    requires std::is_class_v<value_type> &&
+             std::three_way_comparable<value_type>
+  {
+    return lhs <=> static_cast<value_type>(rhs);
+  }
+
+  friend auto operator<=>(basic_index_based_proxy const& lhs,
+                          basic_index_based_proxy const& rhs)
+      -> std::compare_three_way_result_t<value_type>
+    requires std::is_class_v<value_type> &&
+             std::three_way_comparable<value_type>
+  {
+    return static_cast<value_type>(lhs) <=> static_cast<value_type>(rhs);
+  }
+
  private:
   container_type& vec_;
   size_type i_;
 };
+
+template <typename Container, std::size_t I>
+using index_based_field_proxy =
+    basic_index_based_proxy<proxy_field_accessor_policy<Container, I>>;
+
+template <typename Container>
+using index_based_value_proxy =
+    basic_index_based_proxy<proxy_value_accessor_policy<Container>>;
 
 } // namespace dwarfs::container::detail
