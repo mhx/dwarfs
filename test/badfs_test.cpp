4d4c48548f5adf0c9c1efda9852fed4029e32249
Marcus Holland-Moritz <github@mhxnet.de>
2026-07-16 13:44:54 +0200

    test(badfs): no longer need to skip bad file system tests

--- test/badfs_test.cpp
+++ test/badfs_test.cpp
@@ -22,7 +22,6 @@
  */
 
 #include <algorithm>
-#include <array>
 #include <filesystem>
 #include <random>
 #include <sstream>
@@ -41,41 +40,6 @@ using namespace dwarfs;
 namespace {
 
 using namespace std::string_view_literals;
-
-constexpr std::array kSkipOn32Bit = {
-    "0161bfabd70ee4d3700a46dbe0bf2335.dwarfs"sv,
-    "0ca44aa3dda67fe9e9dd85bafbcf8c65.dwarfs"sv,
-    "1ee0685c6ec60cc83d204dcd2a86cf6e.dwarfs"sv,
-    "288e74070d7e82ba12a6c7f87c7b74c2.dwarfs"sv,
-    "320da6d7bce5948ef356e4fe01b20275.dwarfs"sv,
-    "38528a6800d8907065e9bc3de6545030.dwarfs"sv,
-    "3935bf683501ba8e0812b96a32f9e9c1.dwarfs"sv,
-    "3cdd36c5bfdcad8f1cb11f3757b10e0d.dwarfs"sv,
-    "67eb016e1ec15aef9e50ddac8119544f.dwarfs"sv,
-    "72028fdf38bc8bf5767467a8eb33cea1.dwarfs"sv,
-    "80c6ae30d257cf7a936eafa54c85e0f4.dwarfs"sv,
-    "af9384d3fac4850ed2f10125b5db730c.dwarfs"sv,
-    "b5c4dfdbba53dda0eea180ae3acccebc.dwarfs"sv,
-    "ccbfc9eb10aa7b89138996ab90a172a1.dwarfs"sv,
-    "f93cd8ed5de226bca0ecefc521df9f13.dwarfs"sv,
-};
-
-#ifdef DWARFS_TEST_RUNNING_ON_ASAN
-constexpr std::array kSkipWithAsan = {
-    "02064956b00513713fde656f9738fc17.dwarfs"sv,
-    "29351be64bffd8bd07f8f1943c8869fd.dwarfs"sv,
-    "2e68f4eb874ea525200d2566c2265af6.dwarfs"sv,
-    "2f6193322fe8ca159229be308ed71399.dwarfs"sv,
-    "35a475fba1c80cb40a9816240e935044.dwarfs"sv,
-    "83f03c7abac9eda814d41496bf2ab149.dwarfs"sv,
-    "910764780f74966a91d1120c7bfc67b4.dwarfs"sv,
-    "abb59522034feda17a598a3464704294.dwarfs"sv,
-    "bc90491054b1a3ba11296d73ad763667.dwarfs"sv,
-    "d1d617c7f2d86dcadf2c757b0fdc6133.dwarfs"sv,
-    "d4f117ce06b45c4594a2e17b03db75cc.dwarfs"sv,
-    "e9afadd7d4935680fff771aded537e33.dwarfs"sv,
-};
-#endif
 
 auto const testdata{std::filesystem::path{TEST_DATA_DIR} / "badfs"};
 
@@ -108,18 +72,6 @@ class bad_fs : public ::testing::TestWithParam<std::string> {};
 TEST_P(bad_fs, test) {
   auto const filename = GetParam();
   auto const filepath = testdata / GetParam();
-
-  if constexpr (sizeof(size_t) == 4) {
-    if (std::ranges::find(kSkipOn32Bit, filename) != kSkipOn32Bit.end()) {
-      GTEST_SKIP() << "skipping test for 32-bit systems: " << filename;
-    }
-  }
-
-#ifdef DWARFS_TEST_RUNNING_ON_ASAN
-  if (std::ranges::find(kSkipWithAsan, filename) != kSkipWithAsan.end()) {
-    GTEST_SKIP() << "skipping test for ASAN builds: " << filename;
-  }
-#endif
 
   test::test_logger lgr;
   test::os_access_mock os;
