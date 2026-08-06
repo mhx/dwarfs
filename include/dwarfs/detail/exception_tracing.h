a02494f8d3bfa3a211b8649c4b23e0c795113a75
Marcus Holland-Moritz <github@mhxnet.de>
2026-08-06 18:22:24 +0200

    feat(worker_group): get backtraces for exceptions thrown in worker threads

--- include/dwarfs/detail/exception_tracing.h
+++ include/dwarfs/detail/exception_tracing.h
@@ -26,42 +26,25 @@
  * SPDX-License-Identifier: MIT
  */
 
-#include <clocale>
-#include <cstdlib>
-#include <iostream>
+#pragma once
 
-#include <dwarfs/detail/exception_tracing.h>
-#include <dwarfs/error.h>
-#include <dwarfs/tool/safe_main.h>
-#include <dwarfs/util.h>
+#include <dwarfs/config.h>
 
-namespace dwarfs::tool {
-
-int safe_main(std::function<int(void)> const& fn) {
-  int retval{1};
-  DWARFS_TRY {
-    install_signal_handlers();
-    setup_default_locale();
-#ifdef _WIN32
-    ::_set_abort_behavior(0, _WRITE_ABORT_MSG);
-#endif
-
-    retval = fn();
-  }
-  DWARFS_CATCH(...) {
-    std::cerr << "ERROR: " << exception_str(std::current_exception()) << "\n";
 #ifdef DWARFS_STACKTRACE_ENABLED
-    auto const& stacktrace = cpptrace::from_current_exception();
-#ifdef DWARFS_CPPTRACE_HAS_FORMATTING
-    auto formatter = cpptrace::formatter{}.addresses(
-        cpptrace::formatter::address_mode::object);
-    formatter.print(std::cerr, stacktrace, true);
+#include <cpptrace/from_current.hpp>
+#if __has_include(<cpptrace/formatting.hpp>)
+#include <cpptrace/formatting.hpp>
+#define DWARFS_CPPTRACE_HAS_FORMATTING
+#endif
+#include <cpptrace/version.hpp>
+#if CPPTRACE_VERSION < 10000
+#define DWARFS_TRY CPPTRACE_TRYZ
+#define DWARFS_CATCH CPPTRACE_CATCHZ
 #else
-    stacktrace.print(std::cerr, true);
+#define DWARFS_TRY CPPTRACE_TRY
+#define DWARFS_CATCH CPPTRACE_CATCH
 #endif
+#else
+#define DWARFS_TRY try
+#define DWARFS_CATCH catch
 #endif
-  }
-  return retval;
-}
-
-} // namespace dwarfs::tool
